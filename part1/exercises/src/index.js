61bb52e3989d6e7f7e934df3f0dd6ad62b1628ac
Franco <francocorniglione5@gmail.com>
2023-10-04 20:34:24 -0300

    index stuff

---
+++ part1/exercises/src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content
+        part1={course.parts[0].name}
+        exercises1={course.parts[0].exercises}
+        part2={course.parts[1].name}
+        exercises2={course.parts[1].exercises}
+        part3={course.parts[2].name}
+        exercises3={course.parts[2].exercises}
+      />
+      <Total totalExercises={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
