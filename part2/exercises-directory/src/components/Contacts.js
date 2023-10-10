182a4652210b9ac76ef44c70b1d680e8991720d6
Franco <francocorniglione5@gmail.com>
2023-10-10 12:16:50 -0300

    editing contacts component

--- part2/exercises-directory/src/components/Contacts.js
+++ part2/exercises-directory/src/components/Contacts.js
@@ -1,9 +1,23 @@
 import { Contact } from "./Contact";
 
-export const Contacts = ({ filter, contacts }) => {
+export const Contacts = ({ filter, contacts, deleteHandler }) => {
   const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase().trim()));
 
   return filter.length > 0
-    ? filteredContacts.map(contact => <Contact key={contact.name} name={contact.name} number={contact.number} />)
-    : contacts.map(contact => <Contact key={contact.name} name={contact.name} number={contact.number} />);
+    ? filteredContacts.map(contact => (
+        <Contact
+          key={contact.name}
+          name={contact.name}
+          number={contact.number}
+          deleteHandler={() => deleteHandler(contact)}
+        />
+      ))
+    : contacts.map(contact => (
+        <Contact
+          key={contact.name}
+          name={contact.name}
+          number={contact.number}
+          deleteHandler={() => deleteHandler(contact)}
+        />
+      ));
 };
