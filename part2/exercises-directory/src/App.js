6212733cbf07433e37d970c59ac8218b3d16e4b5
Franco <francocorniglione5@gmail.com>
2023-10-10 12:15:35 -0300

    axios requests and error handling

--- part2/exercises-directory/src/App.js
+++ part2/exercises-directory/src/App.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import "./App.css";
+import { Message } from "./components/Message";
 import { Filter } from "./components/Filter";
 import { ContactForm } from "./components/ContactForm";
 import { Contacts } from "./components/Contacts";
+import contactService from "./services/contacts";
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [message, setMessage] = useState({ error: false, content: "" });
 
   const handleChangeFilter = event => {
     setFilter(event.target.value);
@@ -23,26 +25,75 @@ const App = () => {
     setNumber(event.target.value);
   };
 
-  const handleSubmit = event => {
+  const addContact = event => {
     event.preventDefault();
-    const nameAlreadyAdded = contacts.find(contact => contact.name === name);
-    nameAlreadyAdded
-      ? alert(`${name} is already added to phonebook`)
-      : setContacts(contacts.concat({ name: name, number: number }));
+    const contactAlreadyAdded = contacts.find(contact => contact.name === name);
+
+    if (contactAlreadyAdded) {
+      if (window.confirm(`${name} is already added to phonebook, replace the old number with a new one?`)) {
+        const contactObject = { ...contactAlreadyAdded, number };
+        contactService
+          .update(contactAlreadyAdded.id, contactObject)
+          .then(returnedContact => {
+            setContacts(contacts.map(contact => (contact.id !== returnedContact.id ? contact : returnedContact)));
+            setMessage({ error: false, content: `${returnedContact.name}'s number has been updated successfully` });
+            setTimeout(() => {
+              setMessage({ ...message, content: "" });
+            }, 5000);
+          })
+          .catch(error => {
+            setMessage({ error: true, content: `Information of ${name} has already been removed from server` });
+            setTimeout(() => {
+              setMessage({ ...message, content: "" });
+            }, 5000);
+            console.error("put error:", error);
+          });
+      }
+    } else {
+      const contactObject = { name, number };
+      contactService
+        .create(contactObject)
+        .then(returnedContact => {
+          setContacts(contacts.concat(returnedContact));
+          setMessage({ error: false, content: `${returnedContact.name} has been added successfully` });
+          setTimeout(() => {
+            setMessage({ ...message, content: "" });
+          }, 5000);
+        })
+        .catch(error => console.error("´post error:", error));
+    }
     setName("");
     setNumber("");
   };
 
+  const removeContact = contact => {
+    const { id, name } = contact;
+
+    if (window.confirm(`Delete ${name} ?`)) {
+      contactService
+        .remove(id)
+        .then(() => {
+          setContacts(contacts.filter(contact => contact.id !== id));
+          setMessage({ error: false, content: `${name} has been deleted successfully` });
+          setTimeout(() => {
+            setMessage({ ...message, content: "" });
+          }, 5000);
+        })
+        .catch(error => console.error("delete error:", error));
+    }
+  };
+
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/contacts")
-      .then(response => setContacts(response.data))
-      .catch(error => console.error("axios error:", error));
+    contactService
+      .getAll()
+      .then(initialContacts => setContacts(initialContacts))
+      .catch(error => console.error("get error:", error));
   }, []);
 
   return (
     <>
       <h2>Phonebook</h2>
+      {message.content.length ? <Message message={message} /> : null}
       <Filter filterHandler={handleChangeFilter} filter={filter} />
 
       <h3>Add a new contact</h3>
@@ -51,11 +102,11 @@ const App = () => {
         name={name}
         numberHandler={handleChangeNumber}
         number={number}
-        submitHandler={handleSubmit}
+        submitHandler={addContact}
       />
 
       <h3>Contacts</h3>
-      <Contacts filter={filter} contacts={contacts} />
+      <Contacts filter={filter} contacts={contacts} deleteHandler={removeContact} />
     </>
   );
 };
