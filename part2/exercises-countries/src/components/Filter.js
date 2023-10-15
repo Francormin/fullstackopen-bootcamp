0bff7ab972c3108a7ef4c4174d3d66611ee5b7ce
Franco <francocorniglione5@gmail.com>
2023-10-14 21:09:16 -0300

    adding code explanation with Codeium extension

--- part2/exercises-countries/src/components/Filter.js
+++ part2/exercises-countries/src/components/Filter.js
@@ -1,5 +1,13 @@
+/**
+ * A component for filtering countries by name.
+ *
+ * @param {function} filterHandler - The event handler for filtering.
+ * @param {string} filter - The current filter value.
+ * @returns {JSX.Element} - The JSX element for the filter component.
+ */
 export const Filter = ({ filterHandler, filter }) => (
   <p>
+    {/* Input field for filtering */}
     Find countries by name: <input onChange={filterHandler} value={filter} />
   </p>
 );
