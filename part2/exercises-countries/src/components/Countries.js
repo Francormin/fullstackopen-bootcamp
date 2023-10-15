d7903418652d908aec75a4eaa127adc9bc4d4d26
Franco <francocorniglione5@gmail.com>
2023-10-14 21:08:58 -0300

    adding code explanation with Codeium extension

--- part2/exercises-countries/src/components/Countries.js
+++ part2/exercises-countries/src/components/Countries.js
@@ -1,16 +1,37 @@
 import { CountryInfo } from "./CountryInfo";
 
-export const Countries = ({ countries, toggle }) =>
-  countries.length === 0 ? (
-    <p>No country found</p>
-  ) : countries.length === 1 ? (
-    <CountryInfo country={countries[0]} />
-  ) : countries.length > 10 ? (
-    <p>Too many matches. Please, specify another filter</p>
-  ) : (
-    countries.map(country => (
-      <p key={country?.ccn3}>
-        {country?.name?.common} <button onClick={() => toggle(country)}>Show</button>
-      </p>
-    ))
-  );
+/**
+ * Render a list of countries based on the filter
+ * @param {Array} countries - The array of countries
+ * @param {function} toggle - The function to toggle country details
+ * @param {string} filter - The filter string
+ * @returns {JSX.Element} - The JSX element containing the list of countries
+ */
+export const Countries = ({ countries, toggle, filter }) => {
+  // Check if filter is empty
+  if (!filter.length) {
+    return <p>Please, type something in order to find countries</p>;
+  }
+
+  // Check if filter has value and there are no countries found
+  if (filter?.length && countries?.length === 0) {
+    return <p>No country found</p>;
+  }
+
+  // Check if there is only one country found
+  if (countries?.length === 1) {
+    return <CountryInfo country={countries[0]} />;
+  }
+
+  // Check if there are too many matches
+  if (countries?.length > 10) {
+    return <p>Too many matches. Please, specify another filter</p>;
+  }
+
+  // Render the list of countries
+  return countries.map(country => (
+    <p key={country?.ccn3}>
+      {country?.name?.common} <button onClick={() => toggle(country)}>Show</button>
+    </p>
+  ));
+};
