bff6729f5f86a31e51852a040ace71f31474ccdb
Franco <francocorniglione5@gmail.com>
2023-10-14 21:08:38 -0300

    adding code explanation with Codeium extension

--- part2/exercises-countries/src/App.js
+++ part2/exercises-countries/src/App.js
@@ -5,36 +5,58 @@ import { Filter } from "./components/Filter";
 import { Countries } from "./components/Countries";
 import { CountryInfo } from "./components/CountryInfo";
 
+/**
+ * Renders the main application component.
+ * Fetches and displays a list of countries based on a filter.
+ * Allows the user to view detailed information about a specific country.
+ */
 const App = () => {
+  // Initialize state variables
   const [countries, setCountries] = useState([]);
   const [filter, setFilter] = useState("");
   const [countryInfo, setCountryInfo] = useState({});
 
+  // Handle filter change
   const handleChangeFilter = event => {
     setCountryInfo({});
     setFilter(event.target.value);
   };
 
+  // Toggle country info
   const toggleCountryInfo = country => {
     setCountryInfo(country);
   };
 
+  // Fetch countries data based on filter
   useEffect(() => {
-    axios
-      .get(`https://restcountries.com/v3.1/name/${filter}`)
-      .then(response => {
-        const sortedCountriesByName = response.data.sort((a, b) => (b.name.common < a.name.common ? 1 : -1));
+    const fetchCountries = async filter => {
+      try {
+        // Make API request to fetch countries based on the provided filter
+        const response = await axios.get(`https://restcountries.com/v3.1/name/${filter}`);
+
+        // Sort the countries by name in descending order
+        const sortedCountriesByName = response.data.sort((a, b) => (a.name.common > b.name.common ? 1 : -1));
+
+        // Set the state with the sorted countries
         setCountries(sortedCountriesByName);
-      })
-      .catch(error => setCountries([]));
+      } catch (error) {
+        // If an error occurs, set the state with an empty array
+        setCountries([]);
+      }
+    };
+
+    if (filter.length) fetchCountries(filter);
   }, [filter]);
 
   return (
     <>
+      {/* Render filter component */}
       <Filter filterHandler={handleChangeFilter} filter={filter} />
 
-      <Countries countries={countries} toggle={toggleCountryInfo} />
+      {/* Render countries component */}
+      <Countries countries={countries} toggle={toggleCountryInfo} filter={filter} />
 
+      {/* Render country info component if country info exists */}
       {Object.keys(countryInfo).length > 0 && <CountryInfo country={countryInfo} />}
     </>
   );
