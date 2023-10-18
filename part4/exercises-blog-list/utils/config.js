bba9d0f9c2e3563737869ea2460dffe7cd2c2cf0
Franco <francocorniglione5@gmail.com>
2023-10-18 19:27:04 -0300

    adding more env variables

--- part4/exercises-blog-list/utils/config.js
+++ part4/exercises-blog-list/utils/config.js
@@ -2,5 +2,7 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI_TEST = process.env.MONGODB_URI_TEST;
+const NODE_ENV = process.env.NODE_ENV;
 
-module.exports = { MONGODB_URI, PORT };
+module.exports = { PORT, MONGODB_URI, MONGODB_URI_TEST, NODE_ENV };
