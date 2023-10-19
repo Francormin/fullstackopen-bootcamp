3da7d5bd3c9b288961ac7690f022bbf696d84787
Franco <francocorniglione5@gmail.com>
2023-10-19 15:27:14 -0300

    refactoring from original promises to async/await

--- part4/exercises-blog-list/controllers/blogs.js
+++ part4/exercises-blog-list/controllers/blogs.js
@@ -2,28 +2,31 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/Blog");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
-  response.json(blogs);
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);
+  } catch (error) {
+    next(error);
+  }
 });
 
-blogsRouter.get("/:id", (request, response, next) => {
+blogsRouter.get("/:id", async (request, response, next) => {
   const { id } = request.params;
 
-  Blog.findById(id)
-    .then(blog => {
-      if (blog) response.json(blog);
-      else response.status(404).end();
-    })
-    .catch(error => {
-      next(error);
-    });
+  try {
+    const blog = await Blog.findById(id);
+    return blog ? response.json(blog) : response.status(404).end();
+  } catch (error) {
+    next(error);
+  }
 });
 
 blogsRouter.post("/", async (request, response, next) => {
   if (!request.body.title || !request.body.url)
-    return response.status(400).send({ error: "title and url fields missing" });
+    return response.status(400).send({ error: "title and url fields are both required" });
 
   if (request.body.likes === undefined) request.body.likes = 0;
+
   const blog = new Blog(request.body);
 
   try {
@@ -34,30 +37,28 @@ blogsRouter.post("/", async (request, response, next) => {
   }
 });
 
-// app.put("/:id", (request, response, next) => {
-//   const { id } = request.params;
-//   const { name, number } = request.body;
-
-//   if (!name && !number) return response.status(400).send({ error: "There is nothing to update" });
-
-//   const contactToUpdate = { name, number };
-
-//   Contact.findByIdAndUpdate(id, contactToUpdate, { runValidators: true, new: true })
-//     .then(updatedContact => {
-//       if (updatedContact) response.json(updatedContact);
-//       else response.status(404).end();
-//     })
-//     .catch(error => {
-//       next(error);
-//     });
-// });
-
 blogsRouter.delete("/:id", async (request, response, next) => {
   const { id } = request.params;
 
   try {
-    const deletedContact = await Blog.findByIdAndDelete(id);
-    return deletedContact ? response.status(204).end() : response.status(404).end();
+    const deletedBlog = await Blog.findByIdAndDelete(id);
+    return deletedBlog ? response.status(204).end() : response.status(404).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.put("/:id", async (request, response, next) => {
+  const { id } = request.params;
+  const { title, author, url, likes } = request.body;
+
+  if (!title && !author && !url && !likes) return response.status(400).send({ error: "nothing to update" });
+
+  const blogToUpdate = { title, author, url, likes };
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(id, blogToUpdate, { new: true });
+    return updatedBlog ? response.json(updatedBlog) : response.status(404).end();
   } catch (error) {
     next(error);
   }
