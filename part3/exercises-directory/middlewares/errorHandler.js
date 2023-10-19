40f77b3e62b71f8477bac134649cd0d69450a880
Franco <francocorniglione5@gmail.com>
2023-10-19 17:43:40 -0300

    handling and sending errors

--- part3/exercises-directory/middlewares/errorHandler.js
+++ part3/exercises-directory/middlewares/errorHandler.js
@@ -5,9 +5,9 @@ module.exports = (error, request, response, next) => {
     response.status(400).send({ error: "Malformatted id" });
   } else if (error.name === "ValidationError") {
     if (error.message.includes("name")) {
-      response.status(400).json({ error: "Name must be unique and must have at least 3 letters" });
+      response.status(400).send({ error: "Name must be unique and must have at least 3 letters" });
     } else if (error.message.includes("number")) {
-      response.status(400).json({ error: "Number must be unique and must have at least 8 digits" });
+      response.status(400).send({ error: "Number must be unique and must have at least 8 digits" });
     }
   } else response.status(500).end();
 };
