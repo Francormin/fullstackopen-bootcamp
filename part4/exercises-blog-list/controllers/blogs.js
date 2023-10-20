5c6c94c7ddc987958120ec5c59b07f086563c127
Franco <francocorniglione5@gmail.com>
2023-10-20 10:13:01 -0300

    updating blogs controllers

--- part4/exercises-blog-list/controllers/blogs.js
+++ part4/exercises-blog-list/controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/Blog");
+const User = require("../models/User");
 
-blogsRouter.get("/", async (request, response) => {
+blogsRouter.get("/", async (request, response, next) => {
   try {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate("author", { username: 1, name: 1 });
     response.json(blogs);
   } catch (error) {
     next(error);
@@ -22,15 +23,29 @@ blogsRouter.get("/:id", async (request, response, next) => {
 });
 
 blogsRouter.post("/", async (request, response, next) => {
-  if (!request.body.title || !request.body.url)
-    return response.status(400).send({ error: "title and url fields are both required" });
+  const { title, author, url, likes } = request.body;
 
-  if (request.body.likes === undefined) request.body.likes = 0;
-
-  const blog = new Blog(request.body);
+  if (!title || !author || !url)
+    return response.status(400).send({
+      error: "title, author and url fields are required"
+    });
 
   try {
+    const user = await User.findById(author);
+    if (!user) return response.status(404).end();
+
+    const blog = new Blog({
+      title,
+      author: user._id,
+      url,
+      likes: likes || 0
+    });
+
     const savedBlog = await blog.save();
+
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     response.status(201).json(savedBlog);
   } catch (error) {
     next(error);
@@ -52,9 +67,17 @@ blogsRouter.put("/:id", async (request, response, next) => {
   const { id } = request.params;
   const { title, author, url, likes } = request.body;
 
-  if (!title && !author && !url && !likes) return response.status(400).send({ error: "nothing to update" });
+  if (!title && !author && !url && !likes)
+    return response.status(400).send({
+      error: "nothing to update"
+    });
 
-  const blogToUpdate = { title, author, url, likes };
+  const blogToUpdate = {
+    title,
+    author,
+    url,
+    likes
+  };
 
   try {
     const updatedBlog = await Blog.findByIdAndUpdate(id, blogToUpdate, { new: true });
