07bf5c15dde02c06448647ee26142d9d2f0c7d14
Franco <francocorniglione5@gmail.com>
2023-10-20 10:13:53 -0300

    updating validation errors

--- part4/exercises-blog-list/middlewares/errorHandler.js
+++ part4/exercises-blog-list/middlewares/errorHandler.js
@@ -2,12 +2,18 @@ module.exports = (error, request, response, next) => {
   console.log("message:", error.message);
 
   if (error.name === "CastError") {
-    response.status(400).send({ error: "Malformatted id" });
+    response.status(400).send({
+      error: "Malformatted id"
+    });
   } else if (error.name === "ValidationError") {
-    if (error.message.includes("name")) {
-      response.status(400).json({ error: "Name must be unique and must have at least 3 letters" });
-    } else if (error.message.includes("number")) {
-      response.status(400).json({ error: "Number must be unique and must have at least 8 digits" });
+    if (error.message.includes("username")) {
+      response.status(400).send({
+        error: "Username must be unique and must have at least 3 letters"
+      });
+    } else if (error.message.includes("passwordHash")) {
+      response.status(400).send({
+        error: "PasswordHash must have at least 3 letters"
+      });
     }
   } else response.status(500).end();
 };
