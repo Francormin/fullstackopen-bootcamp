058ace130cad2f08925f9db0e9903ac9a744ae4c
Franco <francocorniglione5@gmail.com>
2023-10-20 10:12:38 -0300

    updating blog schema

--- part4/exercises-blog-list/models/Blog.js
+++ part4/exercises-blog-list/models/Blog.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
-const logger = require("../utils/logger");
-const config = require("../utils/config");
-
-const connectionString = config.NODE_ENV === "test" ? config.MONGODB_URI_TEST : config.MONGODB_URI;
-
-mongoose
-  .connect(connectionString)
-  .then(() => {
-    logger.info("Connected to MongoDB");
-  })
-  .catch(error => {
-    logger.error("Error connecting to MongoDB:", error.message);
-  });
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
 });
 
 blogSchema.set("toJSON", {
