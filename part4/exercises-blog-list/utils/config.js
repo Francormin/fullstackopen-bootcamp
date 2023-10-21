098382795cb1db000b9a2b663d8a8732822c4ed0
Franco <francocorniglione5@gmail.com>
2023-10-21 10:20:24 -0300

    code formatting  stuff

--- part4/exercises-blog-list/utils/config.js
+++ part4/exercises-blog-list/utils/config.js
@@ -5,4 +5,9 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const MONGODB_URI_TEST = process.env.MONGODB_URI_TEST;
 const NODE_ENV = process.env.NODE_ENV;
 
-module.exports = { PORT, MONGODB_URI, MONGODB_URI_TEST, NODE_ENV };
+module.exports = {
+  PORT,
+  MONGODB_URI,
+  MONGODB_URI_TEST,
+  NODE_ENV
+};
