2735fc26cd8a2a21940ed1e07178e59fcbc9f46d
Franco <francocorniglione5@gmail.com>
2023-10-23 11:13:18 -0300

    code formatting stuff

--- part4/exercises-blog-list/controllers/users.js
+++ part4/exercises-blog-list/controllers/users.js
@@ -4,7 +4,12 @@ const User = require("../models/User");
 
 usersRouter.get("/", async (request, response, next) => {
   try {
-    const users = await User.find({}).populate("blogs", { title: 1, url: 1, likes: 1 });
+    const users = await User.find({}).populate("blogs", {
+      title: 1,
+      url: 1,
+      likes: 1
+    });
+
     response.json(users);
   } catch (error) {
     next(error);
