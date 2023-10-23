7ac5fd779f2494715ce71237c0904803df6ace75
Franco <francocorniglione5@gmail.com>
2023-10-23 20:24:13 -0300

    adding new env variable for jsonwebtoken

--- part4/exercises-blog-list/utils/config.js
+++ part4/exercises-blog-list/utils/config.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 const MONGODB_URI_TEST = process.env.MONGODB_URI_TEST;
+const SECRET = process.env.SECRET;
 const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
   PORT,
   MONGODB_URI,
   MONGODB_URI_TEST,
+  SECRET,
   NODE_ENV
 };
