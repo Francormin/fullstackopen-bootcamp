305ea3084272381e7bf2686ff3d1853af8707855
Franco <francocorniglione5@gmail.com>
2023-10-26 17:32:47 -0300

    blog component stuff

--- part5/exercises-blog-list/src/components/Blog.jsx
+++ part5/exercises-blog-list/src/components/Blog.jsx
@@ -1,7 +1,19 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author.name}
-  </div>
-);
+const Blog = ({ blog, userName }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  return (
+    <div style={blogStyle}>
+      <span>
+        {blog.title} {blog.author.name || userName} <button>View</button>
+      </span>
+    </div>
+  );
+};
 
 export default Blog;
