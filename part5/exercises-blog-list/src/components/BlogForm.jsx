0399d0580fadd49b93d3da2a67138453e09d5649
Franco <francocorniglione5@gmail.com>
2023-10-27 15:52:00 -0300

    code refactoring stuff

--- part5/exercises-blog-list/src/components/BlogForm.jsx
+++ part5/exercises-blog-list/src/components/BlogForm.jsx
@@ -1,26 +1,80 @@
+import { useState } from "react";
+import blogService from "../services/blogs";
 import Togglable from "./Togglable";
 
-const BlogForm = ({ title, handleTitleChange, url, handleUrlChange, handleSubmit, message, reference }) => (
-  <Togglable buttonLabel="New blog" ref={reference}>
-    <div>
-      <h2>Create a new blog</h2>
+const BlogForm = ({ createBlog, message, handleMessageChange, reference }) => {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          Title:{" "}
-          <input type="text" value={title} name="Title" onChange={({ target }) => handleTitleChange(target.value)} />
-        </div>
+  const handleTitleChange = event => {
+    setTitle(event.target.value);
+  };
 
-        <div>
-          Url: <input type="text" value={url} name="Url" onChange={({ target }) => handleUrlChange(target.value)} />
-        </div>
+  const handleUrlChange = event => {
+    setUrl(event.target.value);
+  };
 
-        <button type="submit" disabled={!title || !url || message.content}>
-          Create
-        </button>
-      </form>
-    </div>
-  </Togglable>
-);
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      const newBlog = await blogService.create({
+        title,
+        url
+      });
+
+      createBlog(newBlog);
+
+      handleMessageChange({
+        content: `a new blog ${title} by ${newBlog.author.name} added`,
+        error: false
+      });
+
+      setTimeout(() => {
+        handleMessageChange({
+          content: null,
+          error: false
+        });
+      }, 5000);
+
+      setTitle("");
+      setUrl("");
+    } catch (exception) {
+      handleMessageChange({
+        content: exception.response.data.error,
+        error: true
+      });
+
+      setTimeout(() => {
+        handleMessageChange({
+          content: null,
+          error: false
+        });
+      }, 5000);
+    }
+  };
+
+  return (
+    <Togglable buttonLabel="New blog" ref={reference}>
+      <div>
+        <h2>Create a new blog</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            Title: <input type="text" value={title} name="Title" onChange={handleTitleChange} />
+          </div>
+
+          <div>
+            Url: <input type="text" value={url} name="Url" onChange={handleUrlChange} />
+          </div>
+
+          <button type="submit" disabled={!title || !url || message.content}>
+            Create
+          </button>
+        </form>
+      </div>
+    </Togglable>
+  );
+};
 
 export default BlogForm;
