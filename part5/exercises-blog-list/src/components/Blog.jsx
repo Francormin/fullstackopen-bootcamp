901dc42674a05180472b1afcfaa55f6f07c2bdba
Franco <francocorniglione5@gmail.com>
2023-10-29 20:47:07 -0300

    styling stuff

--- part5/exercises-blog-list/src/components/Blog.jsx
+++ part5/exercises-blog-list/src/components/Blog.jsx
@@ -2,25 +2,21 @@ import Togglable from "./Togglable";
 
 const Blog = ({ blog, userName }) => {
   const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: "solid",
-    borderWidth: 1,
+    padding: 10,
+    border: "1px solid",
     marginBottom: 5
   };
 
   return (
     <div style={blogStyle}>
-      <span>
-        {blog.title}
-        <Togglable buttonLabel="View" buttonLabel2="Hide">
-          <p>Url: {blog.url}</p>
-          <p>
-            Likes: {blog.likes} <button>like</button>
-          </p>
-          <p>Author: {blog.author.name || userName}</p>
-        </Togglable>
-      </span>
+      {blog.title}
+      <Togglable buttonLabel="View" buttonLabel2="Hide">
+        <p>Url: {blog.url}</p>
+        <p>
+          Likes: {blog.likes} <button>Like</button>
+        </p>
+        <p>Author: {blog.author.name || userName}</p>
+      </Togglable>
     </div>
   );
 };
