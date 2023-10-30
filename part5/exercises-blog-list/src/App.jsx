e698f779423a0ca2f6ed66ae84ece08f51f5dea4
Franco <francocorniglione5@gmail.com>
2023-10-30 13:29:13 -0300

    updating blog stuff

--- part5/exercises-blog-list/src/App.jsx
+++ part5/exercises-blog-list/src/App.jsx
@@ -21,11 +21,31 @@ const App = () => {
     setBlogs(blogs.concat(newBlog));
   };
 
+  const updateBlog = updatedBlog => {
+    setBlogs(
+      blogs
+        .map(blog => (blog.id !== updatedBlog.id ? blog : updatedBlog))
+        .sort((a, b) => {
+          if (a.likes < b.likes) {
+            return 1;
+          } else if (a.likes > b.likes) {
+            return -1;
+          } else {
+            return 0;
+          }
+        })
+    );
+  };
+
+  const removeBlog = id => {
+    setBlogs(blogs.filter(blog => blog.id !== id));
+  };
+
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
         const blogsInDatabase = await blogService.getAll();
-        setBlogs(blogsInDatabase);
+        setBlogs(blogsInDatabase.sort((a, b) => (a.likes < b.likes ? 1 : -1)));
       } catch (exception) {
         displayMessage(setMessage, exception.response.data.error, true);
       }
@@ -62,7 +82,14 @@ const App = () => {
 
           <br />
           {blogs.map(blog => (
-            <Blog key={blog.id} blog={blog} />
+            <Blog
+              key={blog.id}
+              blog={blog}
+              updateBlog={updateBlog}
+              removeBlog={removeBlog}
+              handleMessageChange={setMessage}
+              user={user}
+            />
           ))}
         </div>
       )}
