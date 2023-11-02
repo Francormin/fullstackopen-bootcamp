537cfb3cf27720f3cf701e5e11705aec5f913386
Franco <francocorniglione5@gmail.com>
2023-11-02 16:55:13 -0300

    cypress commands stuff

--- part5/exercises-blog-list/cypress/support/commands.js
+++ part5/exercises-blog-list/cypress/support/commands.js
@@ -34,6 +34,19 @@ Cypress.Commands.add("login", ({ username, password }) => {
   });
 });
 
+Cypress.Commands.add("createBlog", ({ title, url, likes = 0 }) => {
+  cy.request({
+    url: "http://localhost:3003/api/blogs",
+    method: "POST",
+    body: { title, url, likes },
+    headers: {
+      Authorization: `bearer ${JSON.parse(localStorage.getItem("loggedBlogappUser")).token}`
+    }
+  });
+
+  cy.visit("http://localhost:5173");
+});
+
 Cypress.Commands.add("createBlogCypress", ({ title, url }) => {
   cy.contains("New blog").click();
 
