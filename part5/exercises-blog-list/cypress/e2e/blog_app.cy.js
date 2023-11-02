a934d6863f8a63e1412984d47a622d8e8cc8d91b
Franco <francocorniglione5@gmail.com>
2023-11-02 16:46:46 -0300

    login testing stuff

--- part5/exercises-blog-list/cypress/e2e/blog_app.cy.js
+++ part5/exercises-blog-list/cypress/e2e/blog_app.cy.js
@@ -8,4 +8,24 @@ describe("Blog app", function () {
     cy.get("input[name='password']").should("exist");
     cy.get("button[type='submit']").should("exist");
   });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("input[name='username']").type("mluukkai");
+      cy.get("input[name='password']").type("salainen");
+      cy.get("button[type='submit']").click();
+
+      cy.contains("Matti Luukkainen logged-in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("input[name='username']").type("mluukkai");
+      cy.get("input[name='password']").type("wrong");
+      cy.get("button[type='submit']").click();
+
+      cy.contains("invalid username or password")
+        .and("have.css", "color", "rgb(255, 0, 0)")
+        .and("have.css", "border-style", "solid");
+    });
+  });
 });
