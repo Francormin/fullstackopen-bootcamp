8f454188481d973101e8487b7d31c70788d7eda0
Franco <francocorniglione5@gmail.com>
2023-11-06 10:31:06 -0300

    adding redux reducer stuff

---
+++ part6/exercises-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
+
+const anecdoteSlice = createSlice({
+  name: "anecdotes",
+  initialState: [],
+  reducers: {
+    setAnecdotes(state, action) {
+      return action.payload;
+    },
+    appendAnecdote(state, action) {
+      state.push(action.payload);
+    },
+    voteAnecdote(state, action) {
+      const id = action.payload;
+      const anecdoteToChange = state.find(a => a.id === id);
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      };
+      return state.map(anecdote => (anecdote.id !== id ? anecdote : changedAnecdote));
+    }
+  }
+});
+
+export const { setAnecdotes, appendAnecdote, voteAnecdote } = anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
+export const createAnecdote = content => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch(appendAnecdote(newAnecdote));
+  };
+};
+
+export const likeAnecdote = anecdote => {
+  return async dispatch => {
+    const updatedAnecdote = await anecdoteService.updateLike(anecdote);
+    dispatch(voteAnecdote(updatedAnecdote.id));
+  };
+};
+
+export default anecdoteSlice.reducer;
