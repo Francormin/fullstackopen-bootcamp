6ff0ca5cb0ef72b28b8afa2e443c23291048915a
Franco <francocorniglione5@gmail.com>
2023-11-12 13:38:21 -0300

    spread op refactoring

--- part7/exercises-anecdotes-2/src/App.jsx
+++ part7/exercises-anecdotes-2/src/App.jsx
@@ -92,17 +92,17 @@ const CreateNew = props => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name="content" type={content.type} value={content.value} onChange={content.onChange} />
+          <input {...content} />
         </div>
 
         <div>
           author
-          <input name="author" type={author.type} value={author.value} onChange={author.onChange} />
+          <input {...author} />
         </div>
 
         <div>
           url for more info
-          <input name="info" type={info.type} value={info.value} onChange={info.onChange} />
+          <input {...info} />
         </div>
 
         <button type="submit">create</button>
