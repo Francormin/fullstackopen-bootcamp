8b2e932ca2ee1da8107ce64fede6798fd724772e
Franco <francocorniglione5@gmail.com>
2023-11-16 09:08:12 -0300

    refactoring blog component

--- part7/exercises-blog-list/src/components/Blog.jsx
+++ part7/exercises-blog-list/src/components/Blog.jsx
@@ -1,43 +1,21 @@
 import { useDispatch } from "react-redux";
 
+import { likeBlog, deleteBlog } from "../redux/reducers/blogReducer";
 import { setNotification } from "../redux/reducers/notificationReducer";
-import blogService from "../services/blogs";
-
 import Togglable from "./Togglable";
 
-const Blog = ({ blog, updateBlog, removeBlog, user }) => {
+const Blog = ({ blog, loggedUser }) => {
   const dispatch = useDispatch();
 
   const handleLike = async () => {
-    try {
-      const updatedBlog = await blogService.update(blog.id, {
-        ...blog,
-        likes: blog.likes + 1
-      });
-
-      updateBlog(updatedBlog);
-
-      dispatch(setNotification(`The blog '${blog.title}' by ${blog.author.name} has been liked`, false, 5));
-    } catch (exception) {
-      exception.response?.data.error
-        ? dispatch(setNotification(exception.response.data.error, true, 5))
-        : dispatch(setNotification(exception.message, true, 5));
-    }
+    dispatch(likeBlog(blog));
+    dispatch(setNotification(`The blog '${blog.title}' by ${blog.author.name} has been liked`, false, 5));
   };
 
   const handleRemove = async () => {
-    try {
-      if (window.confirm(`Are you sure you want to delete blog '${blog.title}'?`)) {
-        await blogService.remove(blog.id);
-
-        removeBlog(blog.id);
-
-        dispatch(setNotification(`The blog '${blog.title}' has been deleted successfully`, false, 5));
-      }
-    } catch (exception) {
-      exception.response
-        ? dispatch(setNotification(exception.response.statusText, true, 5))
-        : dispatch(setNotification(exception.message, true, 5));
+    if (window.confirm(`Are you sure you want to delete blog '${blog.title}'?`)) {
+      dispatch(deleteBlog(blog.id));
+      dispatch(setNotification(`The blog '${blog.title}' has been deleted successfully`, false, 5));
     }
   };
 
@@ -56,7 +34,7 @@ const Blog = ({ blog, updateBlog, removeBlog, user }) => {
           Likes: {blog.likes} <button onClick={handleLike}>Like</button>
         </p>
         <p>Author: {blog.author?.name}</p>
-        {blog.author?.name === user?.name && (
+        {blog.author?.name === loggedUser?.name && (
           <button onClick={handleRemove} style={{ backgroundColor: "#0D61E4" }}>
             Delete
           </button>
