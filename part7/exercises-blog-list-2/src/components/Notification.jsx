eddf5f1fbd17c9c862b64cbd989c02b480c7f809
Franco <francocorniglione5@gmail.com>
2023-11-17 11:07:53 -0300

    notification component refactoring

---
+++ part7/exercises-blog-list-2/src/components/Notification.jsx
@@ -0,0 +1,37 @@
+import { useNotificationValue } from "../context/NotificationContext";
+
+const Notification = () => {
+  const notification = useNotificationValue();
+
+  const errorStyle = {
+    color: "red",
+    background: "lightgrey",
+    fontSize: 16,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20
+  };
+
+  const infoStyle = {
+    color: "green",
+    background: "lightgrey",
+    fontSize: 16,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20
+  };
+
+  return (
+    <div>
+      {!notification.content ? null : notification.error ? (
+        <div style={errorStyle}>{notification.content}</div>
+      ) : (
+        <div style={infoStyle}>{notification.content}</div>
+      )}
+    </div>
+  );
+};
+
+export default Notification;
