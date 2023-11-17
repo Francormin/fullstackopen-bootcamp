87f30a60a53910e45d4ca917708e246c9fea2e6c
Franco <francocorniglione5@gmail.com>
2023-11-17 09:49:19 -0300

    loggedUser context stuff

---
+++ part7/exercises-blog-list-2/src/context/LoggedUserContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useReducer } from "react";
+
+const LoggedUserReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_LOGGED_USER":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const LoggedUserContext = createContext();
+
+export const LoggedUserContextProvider = props => {
+  const [loggedUser, loggedUserDispatch] = useReducer(LoggedUserReducer, {});
+
+  return (
+    <LoggedUserContext.Provider value={[loggedUser, loggedUserDispatch]}>{props.children}</LoggedUserContext.Provider>
+  );
+};
+
+export const useLoggedUserValue = () => {
+  const loggedUserAndDispatch = useContext(LoggedUserContext);
+  return loggedUserAndDispatch[0];
+};
+
+export const useLoggedUserDispatch = () => {
+  const loggedUserAndDispatch = useContext(LoggedUserContext);
+  return loggedUserAndDispatch[1];
+};
+
+export default LoggedUserContext;
