60e2d2124b2b76fedb7e58c2d0027ea21a5574af
Franco <francocorniglione5@gmail.com>
2023-11-18 11:09:01 -0300

    first custom hook refactor

---
+++ part7/exercises-blog-list-2/src/hooks/useSpinner.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export const useSpinner = (duration = 1000) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [duration]);
+
+  return isLoading;
+};
