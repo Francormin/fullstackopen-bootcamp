d4c8777ac92a5735dcbf368662aa81edc4218f32
Franco <francocorniglione5@gmail.com>
2023-11-18 11:04:31 -0300

    refactoring app component

--- part7/exercises-blog-list-2/src/App.jsx
+++ part7/exercises-blog-list-2/src/App.jsx
@@ -1,63 +1,57 @@
 import { useEffect } from "react";
-import { useQuery } from "react-query";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-import { getBlogs } from "./services/blogs";
-import { useLoggedUserDispatch, useLoggedUserValue } from "./context/LoggedUserContext";
+import { setLoggedUser, useLoggedUserDispatch, useLoggedUserValue } from "./context/LoggedUserContext";
+import { loggedUserJSON } from "./services/blogs";
 
-import Blog from "./components/Blog";
-import Notification from "./components/Notification";
-import BlogForm from "./components/BlogForm";
 import LoginForm from "./components/LoginForm";
-import Logout from "./components/Logout";
+import Users from "./components/Users";
+import Blogs from "./components/Blogs";
+import User from "./components/User";
+import ErrorPage from "./components/ErrorPage";
+import Home from "./components/Home";
 
 const App = () => {
-  const { data: blogs, status } = useQuery("blogs", getBlogs, {
-    refetchOnWindowFocus: false,
-    retry: 1
-  });
-
   const loggedUser = useLoggedUserValue();
-  const dispatch = useLoggedUserDispatch();
+  const dispatchLogin = useLoggedUserDispatch();
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
-
     if (loggedUserJSON) {
       const loggedUser = JSON.parse(loggedUserJSON);
-
-      dispatch({
-        type: "SET_LOGGED_USER",
-        payload: loggedUser
-      });
+      setLoggedUser(dispatchLogin, loggedUser);
     }
-  }, [dispatch]);
+  }, [dispatchLogin]);
+
+  const inlineStyles = {
+    padding: 20
+  };
 
   return (
     <div>
       {!Object.keys(loggedUser).length ? (
         <LoginForm />
-      ) : status === "error" ? (
-        <div>blog service not available due to problems in server</div>
-      ) : status === "loading" ? (
-        <div>loading data...</div>
       ) : (
-        <div>
-          <h2>Blogs</h2>
+        <BrowserRouter>
+          <header>
+            <Link to="/" style={inlineStyles}>
+              Home
+            </Link>
+            <Link to="/users" style={inlineStyles}>
+              Users
+            </Link>
+            <Link to="/blogs" style={inlineStyles}>
+              Blogs
+            </Link>
+          </header>
 
-          <Notification />
-
-          <p>
-            {loggedUser.name} logged-in <Logout />
-          </p>
-          <BlogForm />
-          <br />
-          {blogs
-            .slice()
-            .sort((a, b) => (a.likes < b.likes ? 1 : -1))
-            .map(blog => (
-              <Blog key={blog.id} blog={blog} />
-            ))}
-        </div>
+          <Routes>
+            <Route path="/" element={<Home loggedUser={loggedUser} />}></Route>
+            <Route path="/users" element={<Users />}></Route>
+            <Route path="/users/:id" element={<User />}></Route>
+            <Route path="/blogs" element={<Blogs />}></Route>
+            <Route path="*" element={<ErrorPage />}></Route>
+          </Routes>
+        </BrowserRouter>
       )}
     </div>
   );
