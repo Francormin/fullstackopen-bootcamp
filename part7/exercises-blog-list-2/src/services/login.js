7c55969f8a1277138691043e74fe8bdfc44334b2
Franco <francocorniglione5@gmail.com>
2023-11-18 11:06:52 -0300

    catching login error

--- part7/exercises-blog-list-2/src/services/login.js
+++ part7/exercises-blog-list-2/src/services/login.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:3003/api/login";
 
-export const login = credentials => axios.post(BASE_URL, credentials).then(res => res.data);
+export const login = credentials =>
+  axios
+    .post(BASE_URL, credentials)
+    .then(res => res.data)
+    .catch(err => err);
