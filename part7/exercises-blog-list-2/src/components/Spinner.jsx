261d067454f9f928e74109b7e26bde5209eb1836
Franco <francocorniglione5@gmail.com>
2023-11-19 12:55:51 -0300

    adding spinner component

---
+++ part7/exercises-blog-list-2/src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import { FaSpinner } from "react-icons/fa";
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div>
+      <FaSpinner className="spinner" />
+    </div>
+  );
+};
+
+export default Spinner;
