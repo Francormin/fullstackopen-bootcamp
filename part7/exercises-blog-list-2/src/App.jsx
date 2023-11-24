7df82b00179c551b03a3f6f5154845af47a1cc52
Franco <francocorniglione5@gmail.com>
2023-11-24 19:59:43 -0300

    app refactoring

--- part7/exercises-blog-list-2/src/App.jsx
+++ part7/exercises-blog-list-2/src/App.jsx
@@ -1,78 +1,45 @@
 import { useEffect } from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import { setLoggedUser, useLoggedUserDispatch, useLoggedUserValue } from "./context/LoggedUserContext";
-import { loggedUserJSON } from "./services/blogs";
+import { setLoggedUser, useLoggedUser } from "./context/LoggedUserContext";
 
 import LoginForm from "./components/LoginForm";
+import Layout from "./components/Layout";
 import Users from "./components/Users";
-import Blogs from "./components/Blogs";
 import User from "./components/User";
+import Blogs from "./components/Blogs";
 import Blog from "./components/Blog";
-import ErrorPage from "./components/ErrorPage";
 import Home from "./components/Home";
-import Logout from "./components/Logout";
-import Notification from "./components/Notification";
 import "./App.css";
 
 const App = () => {
-  const loggedUser = useLoggedUserValue();
-  const dispatchLogin = useLoggedUserDispatch();
+  const loggedUser = useLoggedUser();
+  console.log(loggedUser);
 
-  useEffect(() => {
-    if (loggedUserJSON) {
-      const loggedUser = JSON.parse(loggedUserJSON);
-      setLoggedUser(dispatchLogin, loggedUser);
-    }
-  }, [dispatchLogin]);
+  // useEffect(() => {
+  //   if (loggedUserJSON) {
+  //     const loggedUser = JSON.parse(loggedUserJSON);
+  //     setLoggedUser(dispatchLogin, loggedUser);
+  //   }
+  // }, [dispatchLogin]);
 
   return (
-    <BrowserRouter>
-      {!Object.keys(loggedUser).length ? (
-        <LoginForm />
-      ) : (
-        <div style={{ height: "inherit" }}>
-          <Navbar collapseOnSelect expand="sm" bg="light" data-bs-theme="light">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse>
-              <Nav>
-                <Nav.Link>
-                  <Link to="/" className="navLink">
-                    Home
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link to="/users" className="navLink">
-                    Users
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link to="/blogs" className="navLink">
-                    Blogs
-                  </Link>
-                </Nav.Link>
-                <Navbar.Text>
-                  {loggedUser.name} logged-in <Logout />
-                </Navbar.Text>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+    <div style={{ height: "inherit" }}>
+      <BrowserRouter>
+        <Routes>
+          {loggedUser ? <Route element={<Home />} /> : <Route path={"/login"} element={<LoginForm />} />}
 
-          <Notification />
-
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<LoginForm />}></Route>
-            <Route path="/users" element={<Users />}></Route>
-            <Route path="/users/:id" element={<User />}></Route>
-            <Route path="/blogs" element={<Blogs />}></Route>
-            <Route path="/blogs/:id" element={<Blog />}></Route>
-            <Route path="*" element={<ErrorPage />}></Route>
-          </Routes>
-        </div>
-      )}
-    </BrowserRouter>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="users" element={<Users />} />
+            <Route path="users/:id" element={<User />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="blogs/:id" element={<Blog />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
