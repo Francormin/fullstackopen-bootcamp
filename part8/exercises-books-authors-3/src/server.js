847bdeb9a2262134457b0ebe39d564e0b4353204
Franco <francocorniglione5@gmail.com>
2023-12-07 09:49:25 -0300

    setting context in apollo server

--- part8/exercises-books-authors-3/src/server.js
+++ part8/exercises-books-authors-3/src/server.js
@@ -1,26 +1,41 @@
+require("dotenv").config();
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const { typeDefs, resolvers } = require("./index");
-
-const MONGODB_URI = process.env.MONGODB_URI;
-
-mongoose
-  .connect(MONGODB_URI)
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch(error => {
-    console.log("error connection to MongoDB:", error.message);
-  });
+const User = require("./models/User");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-startStandaloneServer(server, { listen: { port: 4000 } }).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(error => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null;
+
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET);
+      const currentUser = await User.findById(decodedToken.id);
+      return { currentUser };
+    }
+  }
+})
+  .then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  })
+  .catch(error => {
+    console.error("Error starting Apollo Server:", error.message);
+  });
