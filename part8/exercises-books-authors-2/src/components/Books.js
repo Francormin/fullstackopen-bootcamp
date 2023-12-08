700864b2f010eb00653e1846882a89c3e924a7e5
Franco <francocorniglione5@gmail.com>
2023-12-08 12:35:12 -0300

    adding new filter feature stuff

--- part8/exercises-books-authors-2/src/components/Books.js
+++ part8/exercises-books-authors-2/src/components/Books.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
 
 const Books = props => {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [genres, setGenres] = useState([
+    "refactoring",
+    "agile",
+    "patterns",
+    "design",
+    "crime",
+    "classic",
+    "revolution",
+    "programming",
+    "logic",
+    "fantasy",
+    "drama",
+    "sci-fi"
+  ]);
+
   const result = useQuery(ALL_BOOKS);
 
   if (result.loading) {
@@ -18,22 +35,37 @@ const Books = props => {
     <div>
       <h2>books</h2>
 
+      <p>
+        in genre <strong>{selectedGenre === null ? "all genres" : selectedGenre}</strong>
+      </p>
+
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map(a => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter(book => selectedGenre === null || (book.genres && book.genres.includes(selectedGenre)))
+            .map(book => (
+              <tr key={book.title}>
+                <td>{book.title}</td>
+                <td>{book.author.name}</td>
+                <td>{book.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+
+      <div>
+        <button onClick={() => setSelectedGenre(null)}>all genres</button>
+        {genres.map(genre => (
+          <button key={genre} onClick={() => setSelectedGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
