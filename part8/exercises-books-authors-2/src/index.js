4f43229cc721e4822f262190603f1db3b5d1cd02
Franco <francocorniglione5@gmail.com>
2023-12-08 10:57:22 -0300

    setting up context with apollo client

--- part8/exercises-books-authors-2/src/index.js
+++ part8/exercises-books-authors-2/src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import App from "./App";
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("user-token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null
+    }
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000"
+});
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "http://localhost:4000"
-  })
+  link: authLink.concat(httpLink)
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
