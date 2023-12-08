98274fcd8475a4cb6e506f4b591f75ef3d1219e2
Franco <francocorniglione5@gmail.com>
2023-12-08 11:00:00 -0300

    adding component states to handle tokens and errors

--- part8/exercises-books-authors-2/src/App.js
+++ part8/exercises-books-authors-2/src/App.js
@@ -1,25 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useApolloClient } from "@apollo/client";
 
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [error, setError] = useState(null);
+  const [token, setToken] = useState(null);
+
+  const client = useApolloClient();
+
+  const logoutHandler = () => {
+    setToken(null);
+    setPage("authors");
+    setError(null);
+    window.localStorage.removeItem("user-token");
+    client.resetStore();
+  };
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("user-token");
+    if (token) {
+      setToken(token);
+    }
+  }, [setToken]);
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        {token && <button onClick={() => setPage("add")}>add book</button>}
+
+        {!token && <button onClick={() => setPage("login")}>login</button>}
+        {token && <button onClick={logoutHandler}>logout</button>}
       </div>
 
-      <Authors show={page === "authors"} />
+      {error && <div style={{ color: "red" }}>{error}</div>}
+
+      <Authors show={page === "authors"} setError={setError} token={token} />
 
       <Books show={page === "books"} />
 
-      <NewBook show={page === "add"} />
+      <NewBook show={page === "add"} setError={setError} />
+
+      <LoginForm show={page === "login"} setError={setError} setPage={setPage} setToken={setToken} />
     </div>
   );
 };
