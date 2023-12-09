cc10df22982517eda216a7ed3ea15c69811de536
Franco <francocorniglione5@gmail.com>
2023-12-09 16:30:49 -0300

    adding authorTable component

---
+++ part8/exercises-books-authors-2/src/components/AuthorTable.js
@@ -0,0 +1,20 @@
+const AuthorTable = ({ authors }) => (
+  <table>
+    <tbody>
+      <tr>
+        <th>name</th>
+        <th>born</th>
+        <th>books</th>
+      </tr>
+      {authors.map(author => (
+        <tr key={author.name}>
+          <td>{author.name}</td>
+          <td>{author.born}</td>
+          <td>{author.bookCount}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+export default AuthorTable;
