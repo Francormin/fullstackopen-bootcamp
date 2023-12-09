733e6c02fd65e24537445b8f076b28c15fe8cc06
Franco <francocorniglione5@gmail.com>
2023-12-09 16:33:17 -0300

    adding recommendations  component

--- part8/exercises-books-authors-2/src/components/Recommendations.js
+++ part8/exercises-books-authors-2/src/components/Recommendations.js
@@ -1,18 +1,14 @@
-import { useQuery } from "@apollo/client";
-import { ALL_BOOKS, GET_CURRENT_USER } from "../queries";
+import useRecommendationsQuery from "../hooks/useRecommendationsQuery";
+import BookTable from "./BookTable";
 
-const Recommendations = props => {
-  const result = useQuery(ALL_BOOKS);
-  const currentUser = useQuery(GET_CURRENT_USER);
+const Recommendations = ({ show }) => {
+  const { loading, favoriteGenre, recommendedBooks } = useRecommendationsQuery();
 
-  if (result.loading) {
+  if (loading) {
     return <div>loading...</div>;
   }
 
-  const books = result.data?.allBooks || [];
-  const me = currentUser.data?.me || {};
-
-  if (!props.show) {
+  if (!show || !favoriteGenre) {
     return null;
   }
 
@@ -21,27 +17,11 @@ const Recommendations = props => {
       <h2>recommendations</h2>
 
       <p>
-        books in your favorite genre <strong>{me.favoriteGenre}</strong>
+        books in your favorite genre <strong>{favoriteGenre}</strong>
       </p>
 
-      <table>
-        <tbody>
-          <tr>
-            <th>title</th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books
-            .filter(book => book.genres.includes(me.favoriteGenre))
-            .map(book => (
-              <tr key={book.title}>
-                <td>{book.title}</td>
-                <td>{book.author.name}</td>
-                <td>{book.published}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      {/* Extracted BookTable component */}
+      <BookTable books={recommendedBooks} />
     </div>
   );
 };
