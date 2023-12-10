b47d39102e64f1c0975b259d7f83826908317c61
Franco <francocorniglione5@gmail.com>
2023-12-10 15:22:33 -0300

    update authors custom hook

---
+++ part8/exercises-books-authors-2/src/hooks/useUpdateAuthorMutation.js
@@ -0,0 +1,35 @@
+import { useMutation } from "@apollo/client";
+import { UPDATE_AUTHOR } from "../queries";
+
+const useUpdateAuthorMutation = () => {
+  const [updateAuthor] = useMutation(UPDATE_AUTHOR, {
+    // Newer approach
+    update: (cache, response) => {
+      cache.modify({
+        fields: {
+          allAuthors: (existingAuthors = []) => {
+            return existingAuthors.map(author =>
+              author.name === response.data.editAuthor.name ? response.data.editAuthor : author
+            );
+          }
+        }
+      });
+    }
+    // Older approach
+    // update: (cache, response) => {
+    //   cache.updateQuery({ query: ALL_AUTHORS }, ({ allAuthors }) => {
+    //     return {
+    //       allAuthors: allAuthors.map(author =>
+    //         author.name === response.data.editAuthor.name ? response.data.editAuthor : author
+    //       )
+    //     };
+    //   });
+    // }
+  });
+
+  return {
+    updateAuthor
+  };
+};
+
+export default useUpdateAuthorMutation;
