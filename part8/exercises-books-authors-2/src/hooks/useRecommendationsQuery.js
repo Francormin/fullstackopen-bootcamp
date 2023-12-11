ddcdf5667e88222f63147a980c76b4d3ef816a3a
Franco <francocorniglione5@gmail.com>
2023-12-11 18:19:54 -0300

    refactoring recommendations custom hook

--- part8/exercises-books-authors-2/src/hooks/useRecommendationsQuery.js
+++ part8/exercises-books-authors-2/src/hooks/useRecommendationsQuery.js
@@ -2,9 +2,20 @@ import { useQuery } from "@apollo/client";
 import { ALL_BOOKS, GET_CURRENT_USER } from "../queries";
 
 const useRecommendationsQuery = () => {
-  const { loading, data: { allBooks = [] } = {} } = useQuery(ALL_BOOKS);
-  const { data = {} } = useQuery(GET_CURRENT_USER);
-  const { me: { favoriteGenre = "" } = {} } = data;
+  const { loading: booksLoading, data: { allBooks = [] } = {} } = useQuery(ALL_BOOKS);
+  const { loading: userLoading, error: userError, data: userData } = useQuery(GET_CURRENT_USER);
+
+  // Check for errors in user query
+  if (userError) {
+    console.error("Error fetching user data:", userError);
+    return { loading: false, favoriteGenre: "", recommendedBooks: [] };
+  }
+
+  // Extract favoriteGenre after ensuring user data is available
+  const { me: { favoriteGenre = "" } = {} } = userData || {};
+
+  // Check for loading state only when there's actual loading happening
+  const loading = booksLoading || userLoading;
 
   return {
     loading,
