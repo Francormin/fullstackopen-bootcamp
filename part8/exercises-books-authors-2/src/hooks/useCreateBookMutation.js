a835a738cd4113bfa5f1e98f81d68c802a5f3cf4
Franco <francocorniglione5@gmail.com>
2023-12-12 11:09:07 -0300

    cache updating stuff

--- part8/exercises-books-authors-2/src/hooks/useCreateBookMutation.js
+++ part8/exercises-books-authors-2/src/hooks/useCreateBookMutation.js
@@ -1,30 +1,43 @@
 import { useMutation } from "@apollo/client";
-import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from "../queries";
+import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS, BOOKS_BY_GENRE } from "../queries";
 
 const useCreateBookMutation = ({ onCompleted, onError }) => {
   const [createBook] = useMutation(CREATE_BOOK, {
     onCompleted,
     onError,
     update: (cache, response) => {
+      const newBook = response.data.addBook;
+
+      // Update the cache for the ALL_BOOKS query
       cache.updateQuery({ query: ALL_BOOKS }, ({ allBooks }) => {
         return {
-          allBooks: allBooks.concat(response.data.addBook)
+          allBooks: allBooks.concat(newBook)
         };
       });
 
+      // Update the cache for the BOOKS_BY_GENRE query with the specific genre
+      newBook.genres.forEach(genre => {
+        cache.updateQuery({ query: BOOKS_BY_GENRE, variables: { genre } }, ({ allBooks }) => {
+          return {
+            allBooks: allBooks.concat(newBook)
+          };
+        });
+      });
+
+      // Update the cache for the ALL_AUTHORS query
       cache.updateQuery({ query: ALL_AUTHORS }, ({ allAuthors }) => {
-        // Assuming that response.data.addBook.author holds the author information
+        // Assuming that newBook.author holds the author information
         const updatedAuthors = [...allAuthors];
 
         // Check if the author is already in the list
-        const existingAuthor = updatedAuthors.find(author => author.name === response.data.addBook.author);
+        const existingAuthor = updatedAuthors.find(author => author.name === newBook.author);
 
         // If the author exists, increment the book count; otherwise, add a new author
         if (existingAuthor) {
           existingAuthor.bookCount += 1;
         } else {
           updatedAuthors.push({
-            name: response.data.addBook.author,
+            name: newBook.author,
             bookCount: 1
           });
         }
