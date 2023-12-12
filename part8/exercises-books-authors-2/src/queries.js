c9abaf297eb6f15ef92cab112674922ee5da44ab
Franco <francocorniglione5@gmail.com>
2023-12-12 10:09:13 -0300

    naming queries and mutations

--- part8/exercises-books-authors-2/src/queries.js
+++ part8/exercises-books-authors-2/src/queries.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const ALL_AUTHORS = gql`
-  query {
+  query GetAllAuthors {
     allAuthors {
       name
       born
@@ -11,7 +11,7 @@ export const ALL_AUTHORS = gql`
 `;
 
 export const UPDATE_AUTHOR = gql`
-  mutation updateAuthor($name: String!, $setBornTo: Int!) {
+  mutation UpdateAuthor($name: String!, $setBornTo: Int!) {
     editAuthor(name: $name, setBornTo: $setBornTo) {
       name
       born
@@ -20,7 +20,7 @@ export const UPDATE_AUTHOR = gql`
 `;
 
 export const ALL_BOOKS = gql`
-  query {
+  query GetAllBooks {
     allBooks {
       title
       published
@@ -32,8 +32,21 @@ export const ALL_BOOKS = gql`
   }
 `;
 
+export const BOOKS_BY_GENRE = gql`
+  query GetBooksByGenre($genre: String) {
+    allBooks(genre: $genre) {
+      title
+      published
+      author {
+        name
+      }
+      genres
+    }
+  }
+`;
+
 export const CREATE_BOOK = gql`
-  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+  mutation CreateBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
     addBook(title: $title, author: $author, published: $published, genres: $genres) {
       title
       author {
@@ -53,8 +66,8 @@ export const LOGIN = gql`
   }
 `;
 
-export const GET_CURRENT_USER = gql`
-  query {
+export const CURRENT_USER = gql`
+  query GetCurrentUser {
     me {
       username
       favoriteGenre
