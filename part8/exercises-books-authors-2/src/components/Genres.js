466fc85b0cc394b1f9c9147aa063a366831c0c12
Franco <francocorniglione5@gmail.com>
2023-12-12 09:23:42 -0300

    moving hardcoded genres array

--- part8/exercises-books-authors-2/src/components/Genres.js
+++ part8/exercises-books-authors-2/src/components/Genres.js
@@ -1,12 +1,29 @@
-const Genres = ({ genres, onGenreClick }) => (
-  <div>
-    <button onClick={() => onGenreClick(null)}>all genres</button>
-    {genres.map(genre => (
-      <button key={genre} onClick={() => onGenreClick(genre)}>
-        {genre}
-      </button>
-    ))}
-  </div>
-);
+const Genres = ({ onGenreClick }) => {
+  const genres = [
+    "refactoring",
+    "agile",
+    "patterns",
+    "design",
+    "crime",
+    "classic",
+    "revolution",
+    "programming",
+    "logic",
+    "fantasy",
+    "drama",
+    "sci-fi"
+  ];
+
+  return (
+    <div>
+      <button onClick={() => onGenreClick(null)}>all genres</button>
+      {genres.map(genre => (
+        <button key={genre} onClick={() => onGenreClick(genre)}>
+          {genre}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 export default Genres;
