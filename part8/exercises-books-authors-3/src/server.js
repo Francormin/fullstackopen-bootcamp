fc013e5202fbda2376df695d68f996450e07fafa
Franco <francocorniglione5@gmail.com>
2023-12-14 19:12:05 -0300

    reverting server file

--- part8/exercises-books-authors-3/src/server.js
+++ part8/exercises-books-authors-3/src/server.js
@@ -1,101 +1,16 @@
 require("dotenv").config();
 const { ApolloServer } = require("@apollo/server");
-const { expressMiddleware } = require("@apollo/server/express4");
-const { ApolloServerPluginDrainHttpServer } = require("@apollo/server/plugin/drainHttpServer");
-const { makeExecutableSchema } = require("@graphql-tools/schema");
-const { PubSub } = require("graphql-subscriptions");
-const { useServer } = require("graphql-ws/lib/use/ws");
-const { WebSocketServer } = require("ws");
-const { createServer } = require("http");
-const cors = require("cors");
-const express = require("express");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
 const { typeDefs, resolvers } = require("./index");
-const { validateAuth } = require("./utils/validation");
 const User = require("./models/User");
 
-// Create an instance of PubSub
-const pubsub = new PubSub();
-
-// Create the schema, which will be used separately by ApolloServer and
-// the WebSocket server.
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-// Create an Express app and HTTP server; we will attach both the WebSocket
-// server and the ApolloServer to this HTTP server.
-const app = express();
-const httpServer = createServer(app);
-
-// Create our WebSocket server using the HTTP server we just set up.
-const wsServer = new WebSocketServer({
-  server: httpServer,
-  path: "/subscriptions"
-});
-// Save the returned server's info so we can shutdown this server later
-const serverCleanup = useServer({ schema }, wsServer);
-
-// Set up ApolloServer.
 const server = new ApolloServer({
-  schema,
-  plugins: [
-    // Proper shutdown for the HTTP server.
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-
-    // Proper shutdown for the WebSocket server.
-    {
-      async serverWillStart() {
-        return {
-          async drainServer() {
-            await serverCleanup.dispose();
-          }
-        };
-      }
-    }
-  ]
+  typeDefs,
+  resolvers
 });
-
-const startServer = async () => {
-  try {
-    await server.start();
-
-    app.use(
-      "/graphql",
-      cors(),
-      express.json(),
-      expressMiddleware(server, {
-        context: async ({ req }) => {
-          const auth = req ? req.headers.authorization : null;
-
-          if (auth && auth.startsWith("Bearer ")) {
-            const token = auth.substring(7);
-
-            try {
-              const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-              const currentUser = await User.findById(decodedToken.id);
-              return { currentUser, pubsub };
-            } catch (error) {
-              console.error("Error verifying JWT:", error.message);
-              validateAuth();
-            }
-          }
-        }
-      })
-    );
-
-    const PORT = process.env.PORT || 4000;
-    // Now that our HTTP server is fully set up, we can listen to it.
-    httpServer.listen(PORT, () => {
-      console.log(`Server is now running on http://localhost:${PORT}/graphql`);
-    });
-  } catch (error) {
-    console.error("Error starting the server:", error.message);
-    throw error.message;
-  }
-};
-
-startServer();
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -104,5 +19,23 @@ mongoose
   })
   .catch(error => {
     console.error("Error connecting to MongoDB:", error.message);
-    throw error.message;
+  });
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null;
+
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET);
+      const currentUser = await User.findById(decodedToken.id);
+      return { currentUser };
+    }
+  }
+})
+  .then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  })
+  .catch(error => {
+    console.error("Error starting Apollo Server:", error.message);
   });
