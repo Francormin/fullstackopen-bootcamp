d6d5eb160f39165f6ecf04e78ec1f4ba021e50a3
Franco <francocorniglione5@gmail.com>
2023-12-15 20:38:14 -0300

    refactoring server initialization

--- part8/exercises-books-authors-3/src/server.js
+++ part8/exercises-books-authors-3/src/server.js
@@ -1,41 +1,66 @@
 require("dotenv").config();
-const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
+const { ApolloServer } = require("apollo-server-express");
+const { execute, subscribe } = require("graphql");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { PubSub } = require("graphql-subscriptions");
+const { SubscriptionServer } = require("subscriptions-transport-ws");
+const { createServer } = require("http");
+const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
 const { typeDefs, resolvers } = require("./index");
 const User = require("./models/User");
 
+const app = express();
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const pubsub = new PubSub();
+
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-});
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch(error => {
-    console.error("Error connecting to MongoDB:", error.message);
-  });
-
-startStandaloneServer(server, {
-  listen: { port: 4000 },
+  resolvers,
   context: async ({ req }) => {
     const auth = req ? req.headers.authorization : null;
+    let currentUser = null;
 
     if (auth && auth.startsWith("Bearer ")) {
       const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET);
-      const currentUser = await User.findById(decodedToken.id);
-      return { currentUser };
+      currentUser = await User.findById(decodedToken.id);
     }
+
+    return { currentUser, pubsub };
   }
-})
-  .then(({ url }) => {
-    console.log(`Server ready at ${url}`);
-  })
-  .catch(error => {
-    console.error("Error starting Apollo Server:", error.message);
+});
+
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  const httpServer = createServer(app);
+  httpServer.listen({ port: 4000 }, () => {
+    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
   });
+
+  SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe
+    },
+    {
+      server: httpServer,
+      path: server.graphqlPath
+    }
+  );
+
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch(error => {
+      console.error("Error connecting to MongoDB:", error.message);
+    });
+}
+
+startServer();
