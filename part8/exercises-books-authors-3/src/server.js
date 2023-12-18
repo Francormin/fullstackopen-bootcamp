82f8879e008ee89dfc376ce0d85935ba69ba81b5
Franco <francocorniglione5@gmail.com>
2023-12-18 16:09:10 -0300

    fixing graphql subscriptions

--- part8/exercises-books-authors-3/src/server.js
+++ part8/exercises-books-authors-3/src/server.js
@@ -8,39 +8,23 @@ const { WebSocketServer } = require("ws");
 const { createServer } = require("http");
 const cors = require("cors");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
-const { typeDefs, resolvers } = require("./index");
+const { typeDefs, resolvers, pubsub } = require("./index");
 const { connectDatabase } = require("./database");
+const User = require("./models/User");
 
 async function startServer() {
+  const app = express();
+  const httpServer = createServer(app);
+
   const schema = makeExecutableSchema({
     typeDefs,
     resolvers
   });
 
-  const app = express();
-  const httpServer = createServer(app);
-
-  const wsServer = new WebSocketServer({
-    server: httpServer,
-    path: "/graphql"
-  });
-
-  const serverCleanup = useServer({ schema }, wsServer);
-
   const server = new ApolloServer({
     schema,
-    context: async ({ req }) => {
-      const auth = req ? req.headers.authorization : null;
-      let currentUser = null;
-
-      if (auth && auth.startsWith("Bearer ")) {
-        const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET);
-        currentUser = await User.findById(decodedToken.id);
-      }
-
-      return { currentUser };
-    },
     plugins: [
       ApolloServerPluginDrainHttpServer({ httpServer }),
       {
@@ -55,8 +39,37 @@ async function startServer() {
     ]
   });
 
+  // Start Apollo Server first
   await server.start();
-  app.use("/graphql", cors(), express.json(), expressMiddleware(server));
+
+  // Apply middleware before Apollo Server middleware
+  app.use(cors());
+  app.use(express.json());
+
+  // Apply Apollo Server middleware
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null;
+        let currentUser = null;
+
+        if (auth && auth.startsWith("Bearer ")) {
+          const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET);
+          currentUser = await User.findById(decodedToken.id);
+        }
+
+        return { currentUser, pubsub };
+      }
+    })
+  );
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql"
+  });
+
+  const serverCleanup = useServer({ schema }, wsServer);
 
   const PORT = process.env.PORT;
   httpServer.listen(PORT, () => {
