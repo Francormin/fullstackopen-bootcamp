dbf99eea69be625cb96a820cd0726428c860bb4d
Franco <francocorniglione5@gmail.com>
2023-12-18 20:18:10 -0300

    fixing subscription custom hook

--- part8/exercises-books-authors-2/src/hooks/useAddedBookSubscription.js
+++ part8/exercises-books-authors-2/src/hooks/useAddedBookSubscription.js
@@ -1,5 +1,5 @@
 import { useSubscription } from "@apollo/client";
-import { ALL_AUTHORS, BOOK_ADDED } from "../queries";
+import { BOOK_ADDED } from "../queries";
 
 export const useAddedBookSubscription = client => {
   useSubscription(BOOK_ADDED, {
@@ -10,24 +10,27 @@ export const useAddedBookSubscription = client => {
 
       client.cache.modify({
         fields: {
-          allBooks(allBooks) {
-            return allBooks.concat(bookAdded);
+          allBooks(existingBooks = []) {
+            return [...existingBooks, bookAdded];
           },
 
-          allAuthors() {
-            const { allAuthors } = client.readQuery({ query: ALL_AUTHORS });
-            const existingAuthor = allAuthors.find(author => author.name === bookAdded.author.name);
+          allAuthors(existingAuthors = []) {
+            const updatedAuthors = [...existingAuthors];
+            const existingAuthorIndex = updatedAuthors.findIndex(author => author.name === bookAdded.author.name);
 
-            if (!existingAuthor) {
-              allAuthors.push({
+            if (existingAuthorIndex === -1) {
+              updatedAuthors.push({
                 name: bookAdded.author.name,
                 bookCount: 1
               });
             } else {
-              existingAuthor.bookCount += 1;
+              updatedAuthors[existingAuthorIndex] = {
+                ...updatedAuthors[existingAuthorIndex],
+                bookCount: updatedAuthors[existingAuthorIndex].bookCount + 1
+              };
             }
 
-            return allAuthors;
+            return updatedAuthors;
           }
         }
       });
