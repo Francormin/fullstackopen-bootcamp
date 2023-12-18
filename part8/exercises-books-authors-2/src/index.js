9a3ac73ca79750be79dfdeb876add3f460a2eb09
Franco <francocorniglione5@gmail.com>
2023-12-18 19:39:55 -0300

    setting splitLink in entry point

--- part8/exercises-books-authors-2/src/index.js
+++ part8/exercises-books-authors-2/src/index.js
@@ -1,7 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, split } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
 import { setContext } from "@apollo/client/link/context";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
@@ -17,12 +19,34 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000"
+  uri: "http://localhost:4000/graphql"
 });
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000/graphql",
+    shouldRetry: true,
+    retryAttempts: 3
+  })
+);
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === "OperationDefinition" && definition.operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink)
+  link: splitLink
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
