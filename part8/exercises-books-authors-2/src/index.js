f1a7fd885e49ae09e2fa6bb0d90f32e6b5e4fbdc
Franco <francocorniglione5@gmail.com>
2023-12-19 15:12:22 -0300

    cleaning index entry point file

--- part8/exercises-books-authors-2/src/index.js
+++ part8/exercises-books-authors-2/src/index.js
@@ -1,54 +1,13 @@
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, split } from "@apollo/client";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { setContext } from "@apollo/client/link/context";
-import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
-import { createClient } from "graphql-ws";
+import { ApolloProvider } from "@apollo/client";
 import ReactDOM from "react-dom/client";
 
+import { createApolloClient } from "./graphql";
 import App from "./App";
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("user-token");
+// Create Apollo Client
+const client = createApolloClient();
 
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : null
-    }
-  };
-});
-
-const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql"
-});
-
-const wsLink = new GraphQLWsLink(
-  createClient({
-    url: "ws://localhost:4000/graphql",
-    shouldRetry: true,
-    retryAttempts: 3
-  })
-);
-
-// The split function takes three parameters:
-//
-// * A function that's called for each operation to execute
-// * The Link to use for an operation if the function returns a "truthy" value
-// * The Link to use for an operation if the function returns a "falsy" value
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return definition.kind === "OperationDefinition" && definition.operation === "subscription";
-  },
-  wsLink,
-  authLink.concat(httpLink)
-);
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: splitLink
-});
-
+// Render the app
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
     <App />
