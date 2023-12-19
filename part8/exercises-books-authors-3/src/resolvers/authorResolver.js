07e59eb5eec4af739097c1441516722d504e8dd9
Franco <francocorniglione5@gmail.com>
2023-12-19 19:27:16 -0300

    fixing n+1 problem

--- part8/exercises-books-authors-3/src/resolvers/authorResolver.js
+++ part8/exercises-books-authors-3/src/resolvers/authorResolver.js
@@ -1,12 +1,8 @@
-const Book = require("../models/Book");
-
 const authorResolver = {
   name: parent => parent.name,
   id: parent => parent.id,
   born: parent => parent.born,
-  bookCount: async parent => {
-    return (await Book.find({ author: parent.id })).length;
-  }
+  bookCount: parent => parent.bookCount
 };
 
 module.exports = authorResolver;
