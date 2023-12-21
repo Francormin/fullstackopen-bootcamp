6f9ad75f491be35c78549d7ffa47ced6b64a895c
Franco <francocorniglione5@gmail.com>
2023-12-21 19:54:49 -0300

    refactoring function to export it

--- part9/exercises-typescript/bmiCalculator.ts
+++ part9/exercises-typescript/bmiCalculator.ts
@@ -1,65 +1,35 @@
-interface BmiCalculatorValues {
-  height: number;
-  weight: number;
-}
-
-const parseArguments = (args: string[]): BmiCalculatorValues | Error => {
-  if (args.length < 4) throw new Error("Not enough arguments.");
-  if (args.length > 4) throw new Error("Too many arguments.");
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3])
-    };
-  } else {
-    throw new Error("Provided values were not numbers!");
-  }
-};
-
-const calculateBmi = (height: number, weight: number, partialMessage: string): void => {
+export const calculateBmi = (height: number, weight: number): object => {
   const bmi: number = Number((weight / (height / 100) ** 2).toFixed(1));
+  const objResponse: object = {
+    height: `${height}cm`,
+    weight: `${weight}kg`
+  };
 
   switch (true) {
     case bmi < 18.5:
-      console.log(`${partialMessage} Underweight`);
-      return;
+      return {
+        ...objResponse,
+        BMI: "Underweight"
+      };
     case bmi >= 18.5 && bmi < 25.0:
-      console.log(`${partialMessage} Normal (healthy weight)`);
-      return;
+      return {
+        ...objResponse,
+        BMI: "Normal (healthy weight)"
+      };
     case bmi >= 25.0 && bmi < 30.0:
-      console.log(`${partialMessage} Overweight`);
-      return;
+      return {
+        ...objResponse,
+        BMI: "Overweight"
+      };
     case bmi >= 30.0:
-      console.log(`${partialMessage} Obese`);
-      return;
+      return {
+        ...objResponse,
+        BMI: "Obese"
+      };
     default:
-      console.log(`${partialMessage} Error`);
-      return;
+      return {
+        ...objResponse,
+        BMI: "Error"
+      };
   }
 };
-
-try {
-  const result: BmiCalculatorValues | Error = parseArguments(process.argv);
-
-  if (result instanceof Error) throw result;
-
-  const { height, weight } = result;
-
-  calculateBmi(
-    height,
-    weight,
-    `
-      Height: ${height}cm
-      Weight: ${weight}kg
-      BMI result:`
-  );
-} catch (error: unknown) {
-  let errorMessage: string = "Something bad happened.";
-
-  if (error instanceof Error) {
-    errorMessage += ` Error: ${error.message}`;
-  }
-
-  console.log(errorMessage);
-}
