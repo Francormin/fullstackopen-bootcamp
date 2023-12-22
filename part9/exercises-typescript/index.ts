93729082569b8a7917e929ed654f658432b1647f
Franco <francocorniglione5@gmail.com>
2023-12-22 09:30:28 -0300

    refactoring types stuff

--- part9/exercises-typescript/index.ts
+++ part9/exercises-typescript/index.ts
@@ -1,20 +1,20 @@
-import express, { Application, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { calculateBmi } from "./bmiCalculator";
 
-const app: Application = express();
+const app = express();
 
 app.get("/hello", (_req: Request, res: Response) => {
   res.send("Hello Full Stack!");
 });
 
 app.get("/bmi", (req: Request, res: Response) => {
-  const height: number = Number(req.query.height);
-  const weight: number = Number(req.query.weight);
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
 
   if (isNaN(height) || isNaN(weight)) {
     res.send({ error: "malformatted parameters" });
   } else {
-    const result: object = calculateBmi(height, weight);
+    const result = calculateBmi(height, weight);
     res.json(result);
   }
 });
