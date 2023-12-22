a7deda25b6cadc3c16105eeff00da0c3f2059291
Franco <francocorniglione5@gmail.com>
2023-12-22 18:11:45 -0300

    adding new endpoint to index

--- part9/exercises-typescript/index.ts
+++ part9/exercises-typescript/index.ts
@@ -1,7 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import express, { Request, Response } from "express";
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req: Request, res: Response) => {
   res.send("Hello Full Stack!");
@@ -12,10 +19,28 @@ app.get("/bmi", (req: Request, res: Response) => {
   const weight = Number(req.query.weight);
 
   if (isNaN(height) || isNaN(weight)) {
-    res.send({ error: "malformatted parameters" });
+    res.status(400).send({ error: "malformatted parameters" });
   } else {
     const result = calculateBmi(height, weight);
-    res.json(result);
+    res.send(result);
+  }
+});
+
+app.post("/exercises", (req: Request, res: Response): Response => {
+  let { target, daily_exercises } = req.body;
+
+  if (!target || !daily_exercises) {
+    return res.status(400).send({ error: "parameters missing" });
+  }
+
+  target = Number(target);
+  daily_exercises = Array.isArray(daily_exercises) ? daily_exercises.map((element: any) => Number(element)) : [];
+
+  if (isNaN(target) || daily_exercises.some((element: number) => isNaN(element))) {
+    return res.status(400).send({ error: "malformatted parameters" });
+  } else {
+    const result = calculateExercises(daily_exercises, target);
+    return res.send(result);
   }
 });
 
