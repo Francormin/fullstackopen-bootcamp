3551aa11adf89eab6170c0b3d48cbca494943b26
Franco <francocorniglione5@gmail.com>
2023-12-24 14:01:28 -0300

    data diagnoses stuff

--- part9/exercises-patientor/src/data/diagnoses.ts
+++ part9/exercises-patientor/src/data/diagnoses.ts
@@ -1,4 +1,6 @@
-const data = [
+import { Diagnose } from "../types";
+
+const diagnoseEntries: Diagnose[] = [
   {
     code: "M24.2",
     name: "Disorder of ligament",
@@ -75,4 +77,4 @@ const data = [
   }
 ];
 
-export default data;
+export default diagnoseEntries;
