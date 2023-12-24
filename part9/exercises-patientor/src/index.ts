02b5530414e84f5e672d237f83f7bb9852fb6e63
Franco <francocorniglione5@gmail.com>
2023-12-24 14:55:48 -0300

    updating index entry point

--- part9/exercises-patientor/src/index.ts
+++ part9/exercises-patientor/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
-import diagnoseRouter from "./diagnoses";
+
+import diagnoseRouter from "./routes/diagnoses";
+import patientRouter from "./routes/patients";
 
 const app = express();
 app.use(express.json());
@@ -8,12 +10,8 @@ app.use(cors());
 
 const PORT = 3001;
 
-app.get("/api/ping", (_req, res) => {
-  console.log("someone pinged here");
-  res.send("pong");
-});
-
 app.use("/api/diagnoses", diagnoseRouter);
+app.use("/api/patients", patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
