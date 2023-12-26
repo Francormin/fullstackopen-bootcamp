068f514fa42f2e09ecfdf5f087ffca712b1ca1ea
Franco <francocorniglione5@gmail.com>
2023-12-26 10:22:28 -0300

    index small change

--- part9/exercises-patientor/src/index.ts
+++ part9/exercises-patientor/src/index.ts
@@ -5,14 +5,14 @@ import diagnoseRouter from "./routes/diagnoses";
 import patientRouter from "./routes/patients";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-
-const PORT = 3001;
 
 app.use("/api/diagnoses", diagnoseRouter);
 app.use("/api/patients", patientRouter);
 
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
