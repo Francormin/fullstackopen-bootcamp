3a9128ce6c9aed7ddd7249689c7cceb59c0d6131
Franco <francocorniglione5@gmail.com>
2023-12-31 12:40:11 -0300

    refactoring app

--- part9/exercises-ilari-frontend/src/App.tsx
+++ part9/exercises-ilari-frontend/src/App.tsx
@@ -1,17 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { Diary } from "./types";
+import Diaries from "./components/Diaries";
+import NewDiaryForm from "./components/NewDiaryForm";
 import "./App.css";
 
-function App() {
-  const [number, setNumber] = useState(5);
+interface AppState {
+  diaries: Array<Diary>;
+}
 
-  const changeNumber = () => {
-    setNumber(number + 1);
+const INITIAL_STATE = [
+  {
+    id: 1,
+    date: "2017-01-01",
+    weather: "sunny",
+    visibility: "great",
+    comment: "Such a lovely day!"
+  },
+  {
+    id: 2,
+    date: "2017-01-02",
+    weather: "cloudy",
+    visibility: "good"
+  }
+];
+
+function App() {
+  const [diaries, setDiaries] = useState<AppState["diaries"]>([]);
+
+  const handleNewDiary = (newDiary: Diary) => {
+    setDiaries(diaries => [...diaries, newDiary]);
   };
+
+  useEffect(() => {
+    setDiaries(INITIAL_STATE);
+  }, []);
 
   return (
     <div className="App">
-      <div>{number}</div>
-      <button onClick={changeNumber}>Change number</button>
+      <h1>Ilari's Flight Diaries</h1>
+      <Diaries diaries={diaries} />
+      <NewDiaryForm onNewDiary={handleNewDiary} />
     </div>
   );
 }
