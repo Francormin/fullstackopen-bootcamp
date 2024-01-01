05df556c9cefeadfccc217cda2e6ffa44143f94c
Franco <francocorniglione5@gmail.com>
2024-01-01 15:42:15 -0300

    refactoring app component with a new custom hook

--- part9/exercises-ilari-frontend/src/App.tsx
+++ part9/exercises-ilari-frontend/src/App.tsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from "react";
-
-import { Diary } from "./types";
+import useDiaries from "./hooks/useDiaries";
 import Diaries from "./components/Diaries";
 import NewDiaryForm from "./components/NewDiaryForm";
 import "./App.css";
 
-interface AppState {
-  diaries: Array<Diary>;
-}
-
 function App() {
-  const [diaries, setDiaries] = useState<AppState["diaries"]>([]);
-
-  const handleNewDiary = (newDiary: Diary) => {
-    setDiaries(diaries => [...diaries, newDiary]);
-  };
-
-  useEffect(() => {
-    const fetchDiaries = async (): Promise<Array<Diary>> => {
-      const response = await window.fetch("http://localhost:3001/api/diaries");
-      return await response.json();
-    };
-
-    fetchDiaries()
-      .then(diaries => setDiaries(diaries))
-      .catch(error => console.error(error));
-  }, []);
+  const { diaries, handleNewDiary } = useDiaries();
 
   return (
     <div className="App">
