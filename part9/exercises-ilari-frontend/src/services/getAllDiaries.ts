2151d382d49c010d86b5f33434ce8ee90f161a05
Franco <francocorniglione5@gmail.com>
2024-01-03 11:56:22 -0300

    getAllDiaries service refactor stuff

--- part9/exercises-ilari-frontend/src/services/getAllDiaries.ts
+++ part9/exercises-ilari-frontend/src/services/getAllDiaries.ts
@@ -1,6 +1,11 @@
 import { Diary } from "../types";
 
 export const getAllDiaries = async (): Promise<Array<Diary>> => {
-  const response = await window.fetch("http://localhost:3001/api/diaries");
-  return await response.json();
+  try {
+    const response = await window.fetch("http://localhost:3001/api/diaries");
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error("get diaries error");
+  }
 };
