d0824ee72ef76827a5a6f516922be1e741f3f1f3
Franco <francocorniglione5@gmail.com>
2024-01-04 18:03:24 -0300

    changing code stuff from fetch to axios

--- part9/exercises-ilari-frontend/src/services/getAllDiaries.ts
+++ part9/exercises-ilari-frontend/src/services/getAllDiaries.ts
@@ -1,11 +1,16 @@
+import axios from "axios";
 import { Diary } from "../types";
 
 export const getAllDiaries = async (): Promise<Array<Diary>> => {
   try {
-    const response = await window.fetch("http://localhost:3001/api/diaries");
-    return await response.json();
+    const { data } = await axios.get("http://localhost:3001/api/diaries");
+    return data;
   } catch (error) {
-    console.error(error);
-    throw new Error("get diaries error");
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data;
+    } else {
+      console.error(error);
+      throw new Error("get diaries error");
+    }
   }
 };
