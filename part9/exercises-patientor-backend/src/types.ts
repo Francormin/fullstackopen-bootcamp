fe7cb0b0d0e53e16e25f563c24ec15ec3dfa2ca2
Franco <francocorniglione5@gmail.com>
2024-01-15 11:25:13 -0300

    updating types

--- part9/exercises-patientor-backend/src/types.ts
+++ part9/exercises-patientor-backend/src/types.ts
@@ -4,6 +4,12 @@ export interface Diagnose {
   latin?: string;
 }
 
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other"
+}
+
 export interface Patient {
   id: string;
   name: string;
@@ -14,20 +20,44 @@ export interface Patient {
   entries: Entry[];
 }
 
-export interface Entry {
-  // id: string;
-  // description: string;
-  // date: string;
-  // specialist: string;
-  // diagnosisCodes?: Array<Diagnose["code"]>;
-  // type: string;
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose["code"]>;
 }
 
-export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
 }
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge?: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 
 export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 
