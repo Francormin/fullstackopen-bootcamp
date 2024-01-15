8eadcab2216867f20d104ac2963a3622d0790f52
Franco <francocorniglione5@gmail.com>
2024-01-15 15:33:36 -0300

    showing diagnoses names

--- part9/exercises-patientor-frontend/src/components/PatientDetails/index.tsx
+++ part9/exercises-patientor-frontend/src/components/PatientDetails/index.tsx
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import FemaleIcon from "@mui/icons-material/Female";
 import MaleIcon from "@mui/icons-material/Male";
 
-import { Patient } from "../../types";
+import { Diagnosis, Patient } from "../../types";
 import patientService from "../../services/patients";
+import diagnosisService from "../../services/diagnoses";
 
 const PatientDetails = () => {
   const [patient, setPatient] = useState<Patient>();
+  const [diagnoses, setDiagnoses] = useState<Array<Diagnosis>>([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -20,6 +22,24 @@ const PatientDetails = () => {
       getPatient();
     }
   }, [id]);
+
+  useEffect(() => {
+    const getDiagnoses = async () => {
+      const diagnoses = await diagnosisService.getAll();
+      setDiagnoses(diagnoses);
+    };
+
+    getDiagnoses();
+  }, []);
+
+  const getDiagnosisNames = (code: string) => {
+    const diagnosisName = diagnoses.filter(d => d.code === code).map(d => d.name);
+    return (
+      <li key={code}>
+        {code} {diagnosisName}
+      </li>
+    );
+  };
 
   return (
     <div>
@@ -49,15 +69,11 @@ const PatientDetails = () => {
           <div>
             <h3>entries</h3>
             {patient.entries.map(entry => (
-              <div>
+              <div key={entry.id}>
                 <p>
                   {entry.date} <i>{entry.description}</i>
                 </p>
-                <ul>
-                  {entry.diagnosisCodes?.map(code => (
-                    <li>{code}</li>
-                  ))}
-                </ul>
+                {entry.diagnosisCodes && <ul>{entry.diagnosisCodes.map(code => getDiagnosisNames(code))}</ul>}
               </div>
             ))}
           </div>
