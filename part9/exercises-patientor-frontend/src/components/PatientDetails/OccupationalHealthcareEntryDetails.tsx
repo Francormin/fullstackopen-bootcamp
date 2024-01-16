43434c84f603936a364dc4ce2563a3b710ff439d
Franco <francocorniglione5@gmail.com>
2024-01-16 10:30:42 -0300

    adding second entry type component

---
+++ part9/exercises-patientor-frontend/src/components/PatientDetails/OccupationalHealthcareEntryDetails.tsx
@@ -0,0 +1,33 @@
+import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
+import { OccupationalHealthcareEntry } from "../../types";
+
+const OccupationalHealthcareEntryDetails: React.FC<{ entry: OccupationalHealthcareEntry }> = ({ entry }) => {
+  return (
+    <div
+      style={{
+        border: "2px solid black",
+        borderRadius: "5px",
+        padding: "5px 10px",
+        margin: "15px 0px"
+      }}
+    >
+      <p
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "5px"
+        }}
+      >
+        {entry.date} <MedicalInformationIcon /> {entry.employerName}
+      </p>
+      <p>
+        <i>{entry.description}</i>
+      </p>
+      {entry.sickLeave?.startDate && <p>start date: {entry.sickLeave.startDate}</p>}
+      {entry.sickLeave?.endDate && <p>end date: {entry.sickLeave.endDate}</p>}
+      <p>diagnosed by {entry.specialist}</p>
+    </div>
+  );
+};
+
+export default OccupationalHealthcareEntryDetails;
