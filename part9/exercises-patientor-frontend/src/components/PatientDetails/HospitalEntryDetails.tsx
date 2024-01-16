440f440cda6530e1f97965722a1cb73a58e5e65d
Franco <francocorniglione5@gmail.com>
2024-01-16 10:31:02 -0300

    adding third entry type component

---
+++ part9/exercises-patientor-frontend/src/components/PatientDetails/HospitalEntryDetails.tsx
@@ -0,0 +1,33 @@
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import { HospitalEntry } from "../../types";
+
+const HospitalEntryDetails: React.FC<{ entry: HospitalEntry }> = ({ entry }) => {
+  return (
+    <div
+      style={{
+        border: "2px solid black",
+        borderRadius: "5px",
+        padding: "5px 10px",
+        margin: "15px 0px"
+      }}
+    >
+      <p
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "5px"
+        }}
+      >
+        {entry.date} <LocalHospitalIcon />
+      </p>
+      <p>
+        <i>{entry.description}</i>
+      </p>
+      {entry.discharge?.date && <p>discharge date: {entry.discharge.date}</p>}
+      {entry.discharge?.criteria && <p>criteria: {entry.discharge.criteria}</p>}
+      <p>diagnosed by {entry.specialist}</p>
+    </div>
+  );
+};
+
+export default HospitalEntryDetails;
