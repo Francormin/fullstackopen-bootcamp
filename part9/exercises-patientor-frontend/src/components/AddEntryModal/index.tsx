139fe80befbfaf470d3ec20b5527a2818eab9866
Franco <francocorniglione5@gmail.com>
2024-01-22 12:30:53 -0300

    adding radio inputs to render different forms

--- part9/exercises-patientor-frontend/src/components/AddEntryModal/index.tsx
+++ part9/exercises-patientor-frontend/src/components/AddEntryModal/index.tsx
@@ -1,8 +1,10 @@
 import { Dialog, DialogTitle, DialogContent, Divider, Alert } from "@mui/material";
-import { Diagnosis, EntryFormValues } from "../../types";
+import { Diagnosis, EntryFormValues, EntryType } from "../../types";
 import AddEntryForm from "./AddEntryForm";
 
 interface Props {
+  entryType: EntryType | null;
+  onEntryTypeChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   modalOpen: boolean;
   onClose: () => void;
   onSubmit: (values: EntryFormValues) => void;
@@ -10,15 +12,52 @@ interface Props {
   diagnoses: Array<Diagnosis>;
 }
 
-const AddEntryModal = ({ modalOpen, onClose, onSubmit, error, diagnoses }: Props) => (
-  <Dialog fullWidth={true} open={modalOpen} onClose={() => onClose()}>
-    <DialogTitle>Add a new entry</DialogTitle>
-    <Divider />
-    <DialogContent>
-      {error && <Alert severity="error">{error}</Alert>}
-      <AddEntryForm onSubmit={onSubmit} onCancel={onClose} diagnoses={diagnoses} />
-    </DialogContent>
-  </Dialog>
-);
+const AddEntryModal = ({ entryType, onEntryTypeChange, modalOpen, onClose, onSubmit, error, diagnoses }: Props) => {
+  return (
+    <Dialog fullWidth={true} open={modalOpen} onClose={() => onClose()}>
+      <DialogTitle>Add a new entry</DialogTitle>
+      <div>
+        <input
+          type="radio"
+          id="hospital"
+          name="entryType"
+          value={EntryType.Hospital}
+          checked={entryType === EntryType.Hospital}
+          onChange={onEntryTypeChange}
+        />
+        <label htmlFor="hospital">Hospital</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="healthCheck"
+          name="entryType"
+          value={EntryType.HealthCheck}
+          checked={entryType === EntryType.HealthCheck}
+          onChange={onEntryTypeChange}
+        />
+        <label htmlFor="healthCheck">Health Check</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="occupationalHealthcare"
+          name="entryType"
+          value={EntryType.OccupationalHealthcare}
+          checked={entryType === EntryType.OccupationalHealthcare}
+          onChange={onEntryTypeChange}
+        />
+        <label htmlFor="occupationalHealthcare">Occupational Healthcare</label>
+      </div>
+      <Divider />
+      <DialogContent>
+        {error && <Alert severity="error">{error}</Alert>}
+        {entryType !== null ? (
+          <AddEntryForm entryType={entryType} onSubmit={onSubmit} onCancel={onClose} diagnoses={diagnoses} />
+        ) : null}
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default AddEntryModal;
