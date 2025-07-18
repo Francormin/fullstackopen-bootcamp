b1a5e25af1b5feca29a091f034bc88103f4b0827
Franco <francocorniglione5@gmail.com>
2025-07-18 10:49:10 -0300

    defining Blog model with sequelize

---
+++ part13/exercises-postgresql/src/models/blog.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Blog = sequelize.define(
+  "blog",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    author: {
+      type: DataTypes.STRING
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
+  },
+  {
+    timestamps: false // Para evitar columnas createdAt/updatedAt
+  }
+);
+
+module.exports = Blog;
