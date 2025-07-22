f6a22d3af1529a9d90b8b86df127c2557ac6099e
Franco <francocorniglione5@gmail.com>
2025-07-22 14:44:30 -0300

    adding two new middlewares

--- part13/exercises-postgresql/index.js
+++ part13/exercises-postgresql/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const blogRoutes = require("./src/routes/blogRoutes");
 const { connectToDatabase } = require("./src/utils/db");
+const { Blog } = require("./src/models");
 const { PORT } = require("./src/utils/config");
 
 const app = express();
@@ -8,18 +9,40 @@ const app = express();
 app.use(express.json());
 app.use("/api/blogs", blogRoutes);
 
-// Middleware de manejo de errores (después de las rutas)
+// Middleware global para manejar rutas desconocidas
+app.use((_req, res) => {
+  res.status(404).json({ error: "Unknown endpoint" });
+});
+
+// Middleware global para manejar errores
 app.use((err, _req, res, _next) => {
   if (err.name === "SequelizeValidationError") {
-    return res.status(400).json({ error: err.message });
+    const messages = err.errors.map(e => {
+      if (e.message.includes("cannot be null")) {
+        const field = e.path.charAt(0).toUpperCase() + e.path.slice(1);
+        return `${field} is required`;
+      }
+      return e.message;
+    });
+
+    return res.status(400).json({ error: messages.join(", ") });
   }
 
-  console.error(err.message);
+  if (err.name === "SequelizeDatabaseError") {
+    return res.status(400).json({ error: "Invalid input. Check your data." });
+  }
+
+  if (err.name === "BadRequestError" || err.name === "NotFoundError") {
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  console.error("Error handler log: ", err);
   res.status(500).json({ error: "Internal server error" });
 });
 
 const start = async () => {
   await connectToDatabase();
+  await Blog.sync({ alter: true });
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
