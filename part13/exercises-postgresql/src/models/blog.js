e7114b4e4601e15509f5382f8e142a1585987308
Franco <francocorniglione5@gmail.com>
2025-07-22 14:47:10 -0300

    adding sequelize model validations

--- part13/exercises-postgresql/src/models/blog.js
+++ part13/exercises-postgresql/src/models/blog.js
@@ -10,19 +10,43 @@ const Blog = sequelize.define(
       primaryKey: true
     },
     author: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      defaultValue: "",
+      validate: {
+        isString(value) {
+          if (typeof value !== "string") {
+            throw new Error("Author must be a string");
+          }
+        }
+      }
     },
     url: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Url is required" },
+        isUrl: { msg: "Url must be valid" }
+      }
     },
     title: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Title is required" },
+        isString(value) {
+          if (typeof value !== "string") {
+            throw new Error("Title must be a string");
+          }
+        }
+      }
     },
     likes: {
       type: DataTypes.INTEGER,
-      defaultValue: 0
+      defaultValue: 0,
+      validate: {
+        isInt: { msg: "Likes must be a number" },
+        min: { args: [0], msg: "Likes cannot be negative" }
+      }
     }
   },
   {
