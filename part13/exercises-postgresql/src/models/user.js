9ff088774e7e6413f4b00cf1c30077af4d388ed2
Franco <francocorniglione5@gmail.com>
2025-07-23 20:03:31 -0300

    adding User new sequelize model

---
+++ part13/exercises-postgresql/src/models/user.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../utils/db");
+
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    unique: {
+      name: "unique_username_constraint",
+      msg: "That username is already taken"
+    },
+    allowNull: false,
+    validate: {
+      notNull: { msg: "Username is required" },
+      notEmpty: { msg: "Username cannot be empty" },
+      isString(value) {
+        if (typeof value !== "string") {
+          throw new Error("Username must be a string");
+        }
+      }
+    }
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { msg: "Name is required" },
+      notEmpty: { msg: "Name cannot be empty" },
+      isString(value) {
+        if (typeof value !== "string") {
+          throw new Error("Name must be a string");
+        }
+      }
+    }
+  }
+});
+
+module.exports = User;
