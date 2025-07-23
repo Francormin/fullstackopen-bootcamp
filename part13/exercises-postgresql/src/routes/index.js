8e8d07ca813cabc7bab1dd75cc84dc60cf5513c2
Franco <francocorniglione5@gmail.com>
2025-07-23 20:03:05 -0300

    adding routes index file

---
+++ part13/exercises-postgresql/src/routes/index.js
@@ -0,0 +1,9 @@
+const blogRoutes = require("./blogRoutes");
+const userRoutes = require("./userRoutes");
+// const loginRoutes = require("./loginRoutes");
+
+module.exports = {
+  blogRoutes,
+  userRoutes
+  // loginRoutes
+};
