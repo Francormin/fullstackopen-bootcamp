2c5aa1a8aa5c5bd5bc075fa3edcf3aaa8e90d104
Franco <francocorniglione5@gmail.com>
2025-07-24 11:25:12 -0300

    adding user controllers

---
+++ part13/exercises-postgresql/src/controllers/userControllers.js
@@ -0,0 +1,38 @@
+const { User } = require("../models");
+
+const getAll = async (_req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+};
+
+const getById = async (req, res) => res.json(req.user);
+
+const create = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(201).json(user);
+};
+
+const updateById = async (req, res) => {
+  const { username, name } = req.body;
+
+  req.user.username = username ?? req.user.username;
+  req.user.name = name ?? req.user.name;
+
+  await req.user.save();
+  res.json(req.user);
+};
+
+const deleteById = async (req, res) => {
+  await req.user.destroy();
+  res.status(204).end();
+};
+
+const userControllers = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById
+};
+
+module.exports = userControllers;
