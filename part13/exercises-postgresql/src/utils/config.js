d5a42769e783487d9dbb9e74f34627fdc5fffd99
Franco <francocorniglione5@gmail.com>
2025-07-24 15:23:36 -0300

    adding new jwt secret env variable

--- part13/exercises-postgresql/src/utils/config.js
+++ part13/exercises-postgresql/src/utils/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
+  JWT_SECRET: process.env.JWT_SECRET,
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
