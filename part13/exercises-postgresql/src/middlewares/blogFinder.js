38abe9892bf7bea465a58c7a7c81b001f0131d38
Franco <francocorniglione5@gmail.com>
2025-07-25 15:30:20 -0300

    updating blogFinder middleware to check user permissions

--- part13/exercises-postgresql/src/middlewares/blogFinder.js
+++ part13/exercises-postgresql/src/middlewares/blogFinder.js
@@ -1,11 +1,20 @@
 const { Blog } = require("../models");
-const { NotFoundError } = require("../utils/errors");
+const { NotFoundError, UnauthorizedError } = require("../utils/errors");
 const { validateParamId } = require("../utils/validators");
 
 const blogFinder = async (req, _res, next) => {
   validateParamId(req.params.id);
-  req.blog = await Blog.findByPk(req.params.id);
-  if (!req.blog) throw new NotFoundError("Blog not found");
+
+  if (req.decodedToken && req.decodedToken.id) {
+    req.blog = await Blog.findOne({
+      where: { id: req.params.id, userId: req.decodedToken.id }
+    });
+    if (!req.blog) throw new UnauthorizedError("You do not have permission to delete this blog");
+  } else {
+    req.blog = await Blog.findByPk(req.params.id);
+    if (!req.blog) throw new NotFoundError("Blog not found");
+  }
+
   next();
 };
 
