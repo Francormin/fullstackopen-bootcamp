e33514bea66f7082ae2f64e4a530ff2bf845584e
Franco <francocorniglione5@gmail.com>
2025-07-25 15:31:05 -0300

    updating middleware index with new tokenExtractor middleware

--- part13/exercises-postgresql/src/middlewares/index.js
+++ part13/exercises-postgresql/src/middlewares/index.js
@@ -2,10 +2,12 @@ const unknownEndpoint = require("./unknownEndpoint");
 const errorHandler = require("./errorHandler");
 const blogFinder = require("./blogFinder");
 const userFinder = require("./userFinder");
+const tokenExtractor = require("./tokenExtractor");
 
 module.exports = {
   unknownEndpoint,
   errorHandler,
   blogFinder,
-  userFinder
+  userFinder,
+  tokenExtractor
 };
