35572b9305aa427a195f9340967f025f0b0ee157
Franco <francocorniglione5@gmail.com>
2025-07-25 15:25:56 -0300

    setting tokenExtractor middleware in blog routes

--- part13/exercises-postgresql/src/routes/blogRoutes.js
+++ part13/exercises-postgresql/src/routes/blogRoutes.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const { blogControllers } = require("../controllers");
-const { blogFinder } = require("../middlewares");
+const { blogFinder, tokenExtractor } = require("../middlewares");
 
 const router = Router();
 
 router.get("/", blogControllers.getAll);
 router.get("/:id", blogFinder, blogControllers.getById);
-router.post("/", blogControllers.create);
+router.post("/", tokenExtractor, blogControllers.create);
 router.put("/:id", blogFinder, blogControllers.updateById);
-router.delete("/:id", blogFinder, blogControllers.deleteById);
+router.delete("/:id", tokenExtractor, blogFinder, blogControllers.deleteById);
 
 module.exports = router;
