dcfffd65e70320a3d2345640d5a17e588cc05d9b
Franco <francocorniglione5@gmail.com>
2025-07-26 16:46:41 -0300

    error message minor change

--- part13/exercises-postgresql/src/middlewares/blogFinder.js
+++ part13/exercises-postgresql/src/middlewares/blogFinder.js
@@ -9,7 +9,7 @@ const blogFinder = async (req, _res, next) => {
     req.blog = await Blog.findOne({
       where: { id: req.params.id, userId: req.decodedToken.id }
     });
-    if (!req.blog) throw new UnauthorizedError("You do not have permission to delete this blog");
+    if (!req.blog) throw new UnauthorizedError("You are not authorized to access this resource");
   } else {
     req.blog = await Blog.findByPk(req.params.id);
     if (!req.blog) throw new NotFoundError("Blog not found");
