424173aa0240030b4df38597a18daf8c21504a09
Franco <francocorniglione5@gmail.com>
2025-07-30 17:58:48 -0300

    refactoring tokenExtractor middleware

--- part13/exercises-postgresql/src/middlewares/tokenExtractor.js
+++ part13/exercises-postgresql/src/middlewares/tokenExtractor.js
@@ -1,15 +1,21 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const { UnauthorizedError } = require("../utils/errors");
+const { User } = require("../models");
 
-const tokenExtractor = (req, _res, next) => {
+const tokenExtractor = async (req, _res, next) => {
   const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    req.decodedToken = jwt.verify(authorization.substring(7), JWT_SECRET);
-    next();
-  } else {
+
+  if (!authorization || !authorization.toLowerCase().startsWith("bearer "))
     throw new UnauthorizedError("Missing token");
-  }
+
+  const decodedToken = jwt.verify(authorization.substring(7), JWT_SECRET);
+  const user = await User.findByPk(decodedToken.id);
+  if (!user) throw new UnauthorizedError("Invalid or expired token");
+
+  req.decodedToken = decodedToken;
+  req.user = user;
+  next();
 };
 
 module.exports = tokenExtractor;
