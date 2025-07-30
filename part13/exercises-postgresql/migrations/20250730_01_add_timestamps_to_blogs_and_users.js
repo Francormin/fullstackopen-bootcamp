86e103af5725e1713145014acf1929d4b1b6ade3
Franco <francocorniglione5@gmail.com>
2025-07-30 17:53:35 -0300

    adding new migrations folder

---
+++ part13/exercises-postgresql/migrations/20250730_01_add_timestamps_to_blogs_and_users.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.addColumn("blogs", "created_at", {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date() // Necesario si ya hay registros
+    });
+
+    await queryInterface.addColumn("blogs", "updated_at", {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date()
+    });
+
+    await queryInterface.addColumn("users", "created_at", {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date()
+    });
+
+    await queryInterface.addColumn("users", "updated_at", {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date()
+    });
+  },
+
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.removeColumn("blogs", "created_at");
+    await queryInterface.removeColumn("blogs", "updated_at");
+    await queryInterface.removeColumn("users", "created_at");
+    await queryInterface.removeColumn("users", "updated_at");
+  }
+};
