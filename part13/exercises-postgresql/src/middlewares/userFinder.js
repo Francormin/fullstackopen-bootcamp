1c9c31fe72ef8d83fc46b6e661402b0d175dbcd9
Franco <francocorniglione5@gmail.com>
2025-08-02 14:59:08 -0300

    refactoring userFinder middleware with auxiliary functions

--- part13/exercises-postgresql/src/middlewares/userFinder.js
+++ part13/exercises-postgresql/src/middlewares/userFinder.js
@@ -1,26 +1,13 @@
-const { User, Blog } = require("../models");
-const { NotFoundError } = require("../utils/errors");
 const { validateParamId } = require("../utils/validators");
+const { findUserById } = require("../utils/queries/userQueries");
 
 const userFinder = (options = {}) => {
   return async (req, _res, next) => {
     validateParamId(req.params.id);
 
-    const queryOptions = options.includeReadingList
-      ? {
-          attributes: ["name", "username"],
-          include: {
-            model: Blog,
-            as: "saved_blogs",
-            through: { attributes: [] }, // Para no incluir datos de la tabla intermedia
-            attributes: ["id", "url", "title", "author", "likes", "year"]
-          }
-        }
-      : undefined;
-
-    const user = await User.findByPk(req.params.id, queryOptions);
-    if (!user) throw new NotFoundError("User not found");
+    const user = await findUserById(req.params.id, options.includeReadingList);
     req.user = user;
+
     next();
   };
 };
