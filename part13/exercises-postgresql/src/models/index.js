03228b93b94ac6da93fd1eec0264e940460a9909
Franco <francocorniglione5@gmail.com>
2025-08-03 17:46:44 -0300

    syncing Sequelize JS code with PostgreSQL migrations

--- part13/exercises-postgresql/src/models/index.js
+++ part13/exercises-postgresql/src/models/index.js
@@ -4,13 +4,31 @@ const ReadingList = require("./readingList");
 
 // Relación uno a muchos
 // Un usuario puede tener muchos blogs, pero un blog pertenece a un solo usuario
-User.hasMany(Blog);
-Blog.belongsTo(User);
+User.hasMany(Blog, {
+  foreignKey: "userId",
+  onDelete: "CASCADE"
+});
+
+Blog.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE"
+});
 
 // Relación muchos a muchos
 // Un usuario puede leer muchos blogs y un blog puede ser leído por muchos usuarios
-User.belongsToMany(Blog, { through: ReadingList, as: "savedBlogs" });
-Blog.belongsToMany(User, { through: ReadingList, as: "savedByUsers" });
+User.belongsToMany(Blog, {
+  through: ReadingList,
+  as: "savedBlogs",
+  foreignKey: "userId",
+  onDelete: "CASCADE"
+});
+
+Blog.belongsToMany(User, {
+  through: ReadingList,
+  as: "savedByUsers",
+  foreignKey: "blogId",
+  onDelete: "CASCADE"
+});
 
 module.exports = {
   Blog,
