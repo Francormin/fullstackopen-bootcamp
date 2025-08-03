4954cba1fc9e84588da2d44dedfc1299e60c6924
Franco <francocorniglione5@gmail.com>
2025-08-03 15:34:46 -0300

    fixing userFinder function call

--- part13/exercises-postgresql/src/routes/userRoutes.js
+++ part13/exercises-postgresql/src/routes/userRoutes.js
@@ -1,13 +1,18 @@
 const { Router } = require("express");
 const { userControllers } = require("../controllers");
-const { userFinder } = require("../middlewares");
+const { userFinder, validateParamId } = require("../middlewares");
 
 const router = Router();
 
 router.get("/", userControllers.getAll);
-router.get("/:id", userFinder({ includeReadingList: true }), userControllers.getById);
+router.get(
+  "/:id",
+  validateParamId,
+  userFinder({ includeReadingList: true }),
+  userControllers.getById
+);
 router.post("/", userControllers.create);
-router.put("/:id", userFinder, userControllers.updateById);
-router.delete("/:id", userFinder, userControllers.deleteById);
+router.put("/:id", validateParamId, userFinder(), userControllers.updateById);
+router.delete("/:id", validateParamId, userFinder(), userControllers.deleteById);
 
 module.exports = router;
