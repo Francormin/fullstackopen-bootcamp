ab61ff2391b599c5d6f9751ab3be981c6aedcbe5
Franco <francocorniglione5@gmail.com>
2025-08-05 13:14:28 -0300

    including possible query param in userFinder middleware

--- part13/exercises-postgresql/src/middlewares/userFinder.js
+++ part13/exercises-postgresql/src/middlewares/userFinder.js
@@ -2,7 +2,12 @@ const { findUserById } = require("../utils/queries/userQueries");
 
 const userFinder = (options = {}) => {
   return async (req, _res, next) => {
-    const user = await findUserById(req.params.id, options.includeReadingList);
+    let read;
+
+    if (req.query.read === "true") read = true;
+    else if (req.query.read === "false") read = false;
+
+    const user = await findUserById(req.params.id, read, options.includeReadingList);
     req.user = user;
 
     next();
